2d84d44e5ae47d4aa55aa8940f9d93fdfd36f311
marcus <marcus@b293a2d2-da53-4cb3-b1bf-c591e3edcd7a>
2008-05-15 10:27:24 +0000

    Pull in new facebook repo
    git-svn-id: http://svn.amee.cc@3 b293a2d2-da53-4cb3-b1bf-c591e3edcd7a

---
+++ clients/branches/ameecalc-simple/ameecalcstyles.php
@@ -0,0 +1,47 @@
+<style type="text/css">
+#ameeapp {
+	padding: 8px;
+}
+div.question {
+	font-weight: normal;
+}
+div.question label {
+	font-weight: normal;
+}
+div.fbbox div.boxHeader {
+	border: 1px solid #cccccc;
+	padding: 5px;
+	background-color: #f7f7f7;
+	font-weight: bold;
+	font-size: 12px;
+	color: #3b5998;
+	text-shadow: 0.3em 0.2em 0.5em #999999;
+}
+div.fbbox div.boxBottom {
+	border: 1px solid #cccccc;
+	border-top: none;
+	padding: 8px;
+}
+div.fbbox {
+	margin-top: 0px;
+}
+div.boxBottom img {
+	margin-top: 8px;
+}
+p {
+	margin: 0px;
+}
+#carbonprofile {
+	padding: 18px 8px;
+	background-color: #AA0005;
+	color: white;
+	font-size: 4em;
+	font-weight: bold;
+	text-align: center;
+	text-shadow: 0.0em 0.0em 1.0em #FFFFC2;
+	margin: 0px;
+}
+#carbonprofilevalue {
+	font-size: 2em;
+}
+</style>
---
+++ clients/branches/ameecalc-simple/tos.php
@@ -0,0 +1,7 @@
+<h1>AMEE Carbon Calculator Terms of Service</h1>
+<p>By installing the AMEE Carbon Calculator application, you accept these terms of service.</p>
+<p>Facebook's terms cover the basics, but essentially be nice, don't try to break anything, don't steal data or code.</p>
+<p>We offer no guarantee of AMEE Carbon Calculator's fitness for purpose, and data originating from it may be subject to error. Any use of AMEE Carbon Calculator is entirely at your own risk and we offer no liability.</p>
+<p>AMEE Carbon Calculator is &copy; copyright 2008 d::gen network limited.</p>
+<p>The AMEE web service is &copy; copyright 2008 d::gen network limited.</p>
+<p>Please follow these links for further details: <a href="http://wiki.co2.dgen.net/index.php/Copyright">Copyright information</a> | <a href="http://wiki.co2.dgen.net/index.php/AMEE:Privacy_policy">Privacy policy</a></p>
