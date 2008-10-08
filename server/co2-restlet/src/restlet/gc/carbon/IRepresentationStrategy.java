20dfa885d855e1b5b4566e335e573a8960c211f6
Stephen Matthew <steve@jellymold.com>
2008-10-08 12:28:40 +0100

    Refactored JSON/XML representations to Strategy pattern.

---
+++ server/co2-restlet/src/restlet/gc/carbon/IRepresentationStrategy.java
@@ -0,0 +1,32 @@
+package gc.carbon;
+
+import org.json.JSONObject;
+import org.json.JSONException;
+import org.w3c.dom.Element;
+import org.w3c.dom.Document;
+import gc.carbon.profile.BaseProfileResource;
+
+/**
+ * This file is part of AMEE.
+ * <p/>
+ * AMEE is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ * <p/>
+ * AMEE is free software and is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * <p/>
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * <p/>
+ * Created by http://www.dgen.net.
+ * Website http://www.amee.cc
+ */
+public interface IRepresentationStrategy {
+    JSONObject getJSONObject(BaseProfileResource resource)  throws JSONException;
+
+    Element getElement(BaseProfileResource resource, Document document);
+}
---
+++ server/co2-restlet/src/restlet/gc/carbon/profile/BaseProfileResource.java
@@ -0,0 +1,53 @@
+package gc.carbon.profile;
+
+import gc.carbon.path.PathItem;
+
+import java.util.List;
+
+import com.jellymold.utils.BaseResource;
+import com.jellymold.utils.Pager;
+import org.restlet.Context;
+import org.restlet.data.Request;
+import org.restlet.data.Response;
+
+/**
+ * This file is part of AMEE.
+ * <p/>
+ * AMEE is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ * <p/>
+ * AMEE is free software and is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * <p/>
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * <p/>
+ * Created by http://www.dgen.net.
+ * Website http://www.amee.cc
+ */
+public abstract class BaseProfileResource extends BaseResource {
+
+    public BaseProfileResource() {
+        super();
+    }
+
+    public BaseProfileResource(Context context, Request request, Response response) {
+        super(context, request, response);
+    }
+
+    public abstract List<ProfileItem> getProfileItems();
+
+    public abstract ProfileItem getProfileItem();
+
+    public abstract ProfileSheetService getProfileSheetService();
+
+    public abstract ProfileBrowser getProfileBrowser();
+
+    public abstract PathItem getPathItem();
+
+    public abstract Pager getPager();
+}
