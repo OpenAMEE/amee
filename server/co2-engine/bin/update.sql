e8c66c77a8a9081191c45c2605db2a2246be8d33
Diggory Briercliffe <dig@MBP-DIG.local>
2008-11-06 15:06:08 +0000

    Updated:

--- server/co2-engine/bin/update.sql
+++ server/co2-engine/bin/update.sql
@@ -1,3 +1,6 @@
-update amee.TARGET set TYPE = 0;
-update amee.TARGET set TYPE = 1 where DIRECTORY_TARGET = 1;
-alter table amee.TARGET drop column DIRECTORY_TARGET;
+update amee.APP set TARGET_BUILDER = '';
+update amee.ENVIRONMENT set ITEMS_PER_FEED = 10;
+update amee.ENVIRONMENT set OWNER = '';
+update amee.USER set LOCATION = '';
+update amee.USER set NICK_NAME = '';
+update amee.USER set USER_TYPE = 0;
