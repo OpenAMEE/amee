f75652750b9e70c815f24e35b5b4e82484940761
Nader Samaan <nadersamaan@nader-samaans-macbook-pro.local>
2009-01-13 17:39:36 +0000

    added 1.1.1 jetty engine parameters

--- server/co2-engine/java/gc/engine/GCEngine.java
+++ server/co2-engine/java/gc/engine/GCEngine.java
@@ -29,10 +29,37 @@ public class GCEngine extends Engine {
         super(initialise, ajpPort, serverName);
     }
 
+    /**
+     * Constructor
+     *
+     * @param initialise               true if engine should be initialised
+     * @param ajpPort                  ajp setting: port
+     * @param maxThreads               Jetty setting: Maximum threads waiting to service requests.
+     * @param minThreads               Jetty setting: Minimum threads that will service requests.
+     * @param threadMaxIdleTimeMs      Jetty setting: Time for an idle thread to wait for a request or read.
+     * @param lowThreads               Jetty setting: Threshold of remaining threads at which the server is considered as running low on resources.
+     * @param lowResourceMaxIdleTimeMs Jetty setting: Time in ms that connections will persist if listener is low on resources.
+     * @param acceptorThreads          Jetty setting: Number of acceptor threads to set.
+     * @param acceptQueueSize          Jetty setting: Size of the accept queue.
+     */
+    public GCEngine(boolean initialise, int ajpPort, String serverName, int maxThreads, int minThreads, int threadMaxIdleTimeMs,
+                    int lowThreads, int lowResourceMaxIdleTimeMs, int acceptorThreads, int acceptQueueSize) {
+        super(initialise, ajpPort, serverName, maxThreads, minThreads, threadMaxIdleTimeMs, lowThreads,
+                lowResourceMaxIdleTimeMs, acceptorThreads, acceptQueueSize);
+    }
+
     public static void main(String[] args) {
 
-        int ajpPort = 8010;
+        int ajpPort = AJP_PORT;
         String serverName = "localhost";
+        int maxThreads = MAX_THREADS;
+        int minThreads = MIN_THREADS;
+        int threadMaxIdleTimeMs = THREAD_MAX_IDLE_TIME_MS;
+        int lowThreads = LOW_THREADS;
+        int lowResourceMaxIdleTimeMs = LOW_RESOURCE_MAX_IDLE_TIME_MS;
+        int acceptorThreads = ACCEPTOR_THREADS;
+        int acceptQueueSize = ACCEPT_QUEUE_SIZE;
+
         Options options = new Options();
 
         Option serverNameOpt = OptionBuilder.withArgName("serverName")
@@ -48,6 +75,56 @@ public class GCEngine extends Engine {
                 .create("ajpPort");
         ajpPortOpt.setRequired(false);
         options.addOption(ajpPortOpt);
+
+        // jetty options
+        Option maxThreadsOpt = OptionBuilder.withArgName("maxThreads")
+                .hasArg()
+                .withDescription("Jetty setting: Maximum threads waiting to service requests.")
+                .create("maxThreads");
+        maxThreadsOpt.setRequired(false);
+        options.addOption(maxThreadsOpt);
+
+        Option minThreadsOpt = OptionBuilder.withArgName("minThreads")
+                .hasArg()
+                .withDescription("Jetty setting: Minimum threads waiting to service requests.")
+                .create("minThreads");
+        minThreadsOpt.setRequired(false);
+        options.addOption(minThreadsOpt);
+
+        Option threadMaxIdleTimeMsOpt = OptionBuilder.withArgName("threadMaxIdleTimeMs")
+                .hasArg()
+                .withDescription("Jetty setting: Time for an idle thread to wait for a request or read.")
+                .create("threadMaxIdleTimeMs");
+        threadMaxIdleTimeMsOpt.setRequired(false);
+        options.addOption(threadMaxIdleTimeMsOpt);
+
+        Option lowThreadsOpt = OptionBuilder.withArgName("lowThreads")
+                .hasArg()
+                .withDescription("Jetty setting: Threshold of remaining threads at which the server is considered as running low on resources.")
+                .create("lowThreads");
+        lowThreadsOpt.setRequired(false);
+        options.addOption(lowThreadsOpt);
+
+        Option lowResourceMaxIdleTimeMsOpt = OptionBuilder.withArgName("lowResourceMaxIdleTimeMs")
+                .hasArg()
+                .withDescription("Jetty setting: Time in ms that connections will persist if listener is low on resources.")
+                .create("lowResourceMaxIdleTimeMs");
+        lowResourceMaxIdleTimeMsOpt.setRequired(false);
+        options.addOption(lowResourceMaxIdleTimeMsOpt);
+
+        Option acceptorThreadsOpt = OptionBuilder.withArgName("acceptorThreads")
+                .hasArg()
+                .withDescription("Jetty setting: Number of acceptor threads to set.")
+                .create("acceptorThreads");
+        acceptorThreadsOpt.setRequired(false);
+        options.addOption(acceptorThreadsOpt);
+
+        Option acceptQueueSizeOpt = OptionBuilder.withArgName("acceptQueueSize")
+                .hasArg()
+                .withDescription("Jetty setting: Size of the accept queue.")
+                .create("acceptQueueSize");
+        acceptQueueSizeOpt.setRequired(false);
+        options.addOption(acceptQueueSizeOpt);
 
         CommandLine line = null;
         CommandLineParser parser = new GnuParser();
@@ -72,7 +149,82 @@ public class GCEngine extends Engine {
             }
         }
 
-        Engine.start(new GCEngine(false, ajpPort, serverName), args);
+        // get jetty options
+        if (line.hasOption(maxThreadsOpt.getOpt())) {
+            try {
+                maxThreads = Integer.parseInt(line.getOptionValue(maxThreadsOpt.getOpt()));
+            } catch (NumberFormatException e) {
+                new HelpFormatter().printHelp("java gc.engine.GCEngine", options);
+                System.exit(-1);
+            }
+        }
+
+        if (line.hasOption(minThreadsOpt.getOpt())) {
+            try {
+                minThreads = Integer.parseInt(line.getOptionValue(minThreadsOpt.getOpt()));
+            } catch (NumberFormatException e) {
+                new HelpFormatter().printHelp("java gc.engine.GCEngine", options);
+                System.exit(-1);
+            }
+        }        
+
+        if (line.hasOption(threadMaxIdleTimeMsOpt.getOpt())) {
+            try {
+                threadMaxIdleTimeMs = Integer.parseInt(line.getOptionValue(threadMaxIdleTimeMsOpt.getOpt()));
+            } catch (NumberFormatException e) {
+                new HelpFormatter().printHelp("java gc.engine.GCEngine", options);
+                System.exit(-1);
+            }
+        }
+
+        if (line.hasOption(lowThreadsOpt.getOpt())) {
+            try {
+                lowThreads = Integer.parseInt(line.getOptionValue(lowThreadsOpt.getOpt()));
+            } catch (NumberFormatException e) {
+                new HelpFormatter().printHelp("java gc.engine.GCEngine", options);
+                System.exit(-1);
+            }
+        }
+
+        if (line.hasOption(lowResourceMaxIdleTimeMsOpt.getOpt())) {
+            try {
+                lowResourceMaxIdleTimeMs = Integer.parseInt(line.getOptionValue(lowResourceMaxIdleTimeMsOpt.getOpt()));
+            } catch (NumberFormatException e) {
+                new HelpFormatter().printHelp("java gc.engine.GCEngine", options);
+                System.exit(-1);
+            }
+        }
+
+        if (line.hasOption(acceptorThreadsOpt.getOpt())) {
+            try {
+                acceptorThreads = Integer.parseInt(line.getOptionValue(acceptorThreadsOpt.getOpt()));
+            } catch (NumberFormatException e) {
+                new HelpFormatter().printHelp("java gc.engine.GCEngine", options);
+                System.exit(-1);
+            }
+        }
+
+        if (line.hasOption(acceptQueueSizeOpt.getOpt())) {
+            try {
+                acceptQueueSize = Integer.parseInt(line.getOptionValue(acceptQueueSizeOpt.getOpt()));
+            } catch (NumberFormatException e) {
+                new HelpFormatter().printHelp("java gc.engine.GCEngine", options);
+                System.exit(-1);
+            }
+        }
+
+        Engine.start(
+                new GCEngine(false,
+                        ajpPort,
+                        serverName,
+                        maxThreads,
+                        minThreads,
+                        threadMaxIdleTimeMs,
+                        lowThreads,
+                        lowResourceMaxIdleTimeMs,
+                        acceptorThreads,
+                        acceptQueueSize),
+                args);
     }
 
     @Override
