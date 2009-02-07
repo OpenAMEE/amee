78da664f6613eaf2b8d08831b4af93c90a2378f2
Stephen Matthew <stephen.matthew@gmail.com>
2009-02-07 17:41:25 +0000

    Added IP address to logging statements. 'Show ATOM' not shown for v1.0 users.

--- projects/utils-restlet/src/main/java/com/jellymold/utils/ThreadBeanHolderFilter.java
+++ projects/utils-restlet/src/main/java/com/jellymold/utils/ThreadBeanHolderFilter.java
@@ -3,14 +3,23 @@ package com.jellymold.utils;
 import org.restlet.Filter;
 import org.restlet.data.Request;
 import org.restlet.data.Response;
+import org.apache.log4j.NDC;
 
 public class ThreadBeanHolderFilter extends Filter {
     protected int doHandle(Request request, Response response) {
+
         try {
+            //Clear the ThreadBeanHolder at the start of each request
             ThreadBeanHolder.clear();
+
+            //Set the NDC (IP address) at the start of each request
+            NDC.push(request.getClientInfo().getAddress());
+
             return super.doHandle(request, response);
+
         } finally {
-            // ThreadBeanHolder.clear();
+            // Clear the NDC when the request has be completed
+            NDC.remove();
         }
     }
 }
