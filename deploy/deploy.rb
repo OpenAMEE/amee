bd1519d96773843f47d41409682b49ed0f4de4d2
Stephen Matthew <stephen.matthew@gmail.com>
2009-03-30 08:20:27 +0100

    Adding support for AWS to deploy process.

--- deploy/deploy.rb
+++ deploy/deploy.rb
@@ -2,6 +2,7 @@ set :stages, %w(development staging production)
 set :default_stage, "development"
 set :stage_dir, "deploy"
 
+require 'capazon'
 require 'capistrano/ext/multistage'
 require 'capistrano/ext/monitor'
 
@@ -34,6 +35,24 @@ set :user, "deploy"
 # Source code and build locations
 set :src_dir, "/Development/AMEE/amee.2.1"
 set :package_dir, "/Development/AMEE/amee.deploy"
+
+#AWS login info
+set :aws_access_key_id, ENV['AMAZON_ACCESS_KEY_ID'] 
+set :aws_secret_access_key, ENV['AMAZON_SECRET_ACCESS_KEY'] 
+
+# Name of the keypair used to spawn and connect to the Amazon EC2 Instance
+# Defaults to one created by the setup_keypair task
+#set :aws_keypair_name, "#{application}-capazon"
+
+# Path to the private key for the Amazon EC2 Instance mentioned above
+# Detaults to one created by setup_keypair task
+#set :aws_private_key_path, "#{Dir.pwd}/#{aws_keypair_name}-key"
+
+#defaults to an ubuntu image
+#set :aws_ami_id, "ami-e4b6538d"
+
+#defaults to, um, default
+#set :aws_security_group, "default"
 
 # Override Capistrano tasks
 deploy.task :start, :roles => :app do
