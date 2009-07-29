9cc8483446b002324e75f02ee9225c68aadc8a66
Stephen Matthew <stephen.matthew@gmail.com>
2009-07-29 19:26:51 +0100

    Latest iteration of deprecation/i18N work.

---
+++ server/amee-domain/src/main/java/com/amee/domain/data/LocaleName.java
@@ -0,0 +1,11 @@
+package com.amee.domain.data;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: steve
+ * Date: Jul 29, 2009
+ * Time: 5:31:52 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class LocaleName {
+}
