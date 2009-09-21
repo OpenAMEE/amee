35d79e47e3acc88bf031ae737288534a4bdce82f
steve matthew <steve@steve-mbp.local>
2009-09-21 12:32:33 +0100

    Deleting incorrectly added artifacts.

--- app/helpers/data_centers_helper.rb
+++
@@ -1,5 +0,0 @@
-module Merb
-  module DataCentersHelper
-
-  end
-end # Merb
--- spec/requests/admin_spec.rb
+++
@@ -1,7 +0,0 @@
-require File.join(File.dirname(__FILE__), '..', 'spec_helper.rb')
-
-describe "/admin" do
-  before(:each) do
-    @response = request("/admin")
-  end
-end
