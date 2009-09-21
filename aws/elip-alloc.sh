2b035dfe4ae3f685c13847db679f6513f29e31bd
steve matthew <steve@steve-mbp.local>
2009-09-21 12:32:33 +0100

    Deleting incorrectly added artifacts.

--- aws/elip-alloc.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# ec2-allocate-address
-# ec2-associate-address -i i-b2e019da  75.101.157.145
-# ec2-describe-addresses
-# ec2-release-address 75.101.157.145
-# ec2-describe-addresses
