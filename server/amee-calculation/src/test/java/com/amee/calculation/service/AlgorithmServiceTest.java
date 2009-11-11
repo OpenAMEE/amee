c92b78b4cd92ab56e0c78ba42f7ba8dc9590800a
Diggory Briercliffe <dig@MBP2-Dig.local>
2009-11-11 11:51:57 +0000

    Minor changes.

--- server/amee-calculation/src/test/java/com/amee/calculation/service/AlgorithmServiceTest.java
+++ server/amee-calculation/src/test/java/com/amee/calculation/service/AlgorithmServiceTest.java
@@ -22,8 +22,6 @@
 package com.amee.calculation.service;
 
 import com.amee.domain.algorithm.Algorithm;
-import static junit.framework.Assert.assertTrue;
-import static junit.framework.Assert.fail;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,6 +31,9 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import javax.script.ScriptException;
 import java.util.HashMap;
 import java.util.Map;
+
+import static junit.framework.Assert.assertTrue;
+import static junit.framework.Assert.fail;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration
