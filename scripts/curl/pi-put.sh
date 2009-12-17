546ef163d94e4c1a64b2621d09fc78b385abfb64
steve matthew <steve@steve-mbp.lan>
2009-12-17 13:31:30 +0000

    Curl script changes - uids etc

--- scripts/curl/pi-put.sh
+++ scripts/curl/pi-put.sh
@@ -5,10 +5,10 @@
 # dataItemUid is the DataItem id NOT the ITEM_DEFINITION id.
 # ValidFrom defaults to 1st day of current month.
 . curl.conf
-data="numberOwned=2"
+data="numberOfPeople=2&representation=full&returnUnit=oz"
 type="xml"
 #amee_url="/profiles/${amee_profile}/home/appliances/computers/generic/056CC619C7D6?method=put"
-amee_url="/profiles/${amee_profile}/home/energy/electricity/F8D1BED7C9A8?method=put"
+amee_url="/profiles/247D8A1820CC/business/buildings/hotel/generic/BD1C37F93B9D?method=put"
 
 curl -H "Accept:application/${type}" \
 	-b .cookies \
