200febf4b734010fe0681970499de9c33e820092
Diggory Briercliffe <dig@MBP2-Dig.local>
2010-02-25 14:14:22 +0000

    Fix error message display for INVALID_RESOURCE_MODIFICATION for PL-272. Add ability to specify Platform time zone for PL-273.

--- server/amee-restlet/src/main/java/com/amee/restlet/utils/APIFault.java
+++ server/amee-restlet/src/main/java/com/amee/restlet/utils/APIFault.java
@@ -39,21 +39,23 @@ public enum APIFault {
     NOT_AUTHORIZED_FOR_INDIRECT_ACCESS;
 
     private String[] strings = {
-            "",
-            "A request was received with one or more invalid parameters.",
-            "A request was received with one or more parameters not supported by the version of the API.",
-            "A request was received with invalid content.",
-            "A request was received with one or more missing parameters.",
-            "A request was received with one or more datetime parameters having an invalid format.",
-            "A request was received with an invalid date range.",
-            "A prorata request was received without a recognised bounded date range.",
-            "A POST or PUT request was received which would have resulted in a duplicate resource being created.",
-            "A request was received with an invalid unit.",
-            "An empty list was received or produced.",
-            "An entity was not found for the given identifier.",
-            "Max batch size was exceeded.",
-            "The DELETE operation must leave at least one ITEM_VALUE per ITEM_VALUE_DEFINTION.",
-            "A request was recieved which is not authorized to access an entity indirectly"};
+            "", // NONE
+            "A request was received with one or more invalid parameters.", // INVALID_PARAMETERS
+            "A request was received with one or more parameters not supported by the version of the API.", // INVALID_API_PARAMETERS
+            "A request was received with invalid content.", // INVALID_CONTENT
+            "A request was received with one or more missing parameters.", // MISSING_PARAMETERS
+            "A request was received with one or more datetime parameters having an invalid format.", // INVALID_DATE_FORMAT
+            "A request was received with an invalid date range.", // INVALID_DATE_RANGE
+            "A prorata request was received without a recognised bounded date range.", // INVALID_PRORATA_REQUEST
+            "A POST or PUT request was received which would have resulted in a duplicate resource being created.", // DUPLICATE_ITEM
+            "A request was received with an invalid unit.", // INVALID_UNIT
+            "An empty list was received or produced.", // EMPTY_LIST
+            "An entity was not found for the given identifier.", // ENTITY_NOT_FOUND
+            "Max batch size was exceeded.", // MAX_BATCH_SIZE_EXCEEDED
+            "The DELETE operation must leave at least one ITEM_VALUE per ITEM_VALUE_DEFINTION.", // DELETE_MUST_LEAVE_AT_LEAST_ONE_ITEM_VALUE
+            "A POST or PUT request was received which would have resulted in an invalid resource modification.", // INVALID_RESOURCE_MODIFICATION
+            "A request was received which is not authorized to access an entity indirectly", // NOT_AUTHORIZED_FOR_INDIRECT_ACCESS
+    };
 
     public String getString() {
         return strings[this.ordinal()];
