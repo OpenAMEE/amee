ff4d5515878f1e8fd9a4d7055817d94cc7894603
David Keen <david.keen@amee.com>
2010-04-19 17:17:57 +0100

    Database migration for time zones.

--- db/dml.sql
+++ db/dml.sql
@@ -1,3 +1,9 @@
 BEGIN;
 
+-- All current users should be Europe/London
+UPDATE USER SET TIME_ZONE = 'Europe/London';
+
+-- Fix the Epoch date
+UPDATE ITEM_VALUE SET START_DATE = '1970-01-01 00:00:00' WHERE START_DATE = '1970-01-01 01:00:00';
+
 COMMIT;
