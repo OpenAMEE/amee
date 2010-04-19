e6694e9b166220d7ec4544450e5ffa7b6693c261
David Keen <david.keen@amee.com>
2010-04-19 17:26:19 +0100

    Set floppy time zone to America/New_York for testing

--- db/dml.sql
+++ db/dml.sql
@@ -3,6 +3,9 @@ BEGIN;
 -- All current users should be Europe/London
 UPDATE USER SET TIME_ZONE = 'Europe/London';
 
+-- Except for testing
+UPDATE USER SET TIME_ZONE = 'America/New_York' WHERE USERNAME IN ('floppy', 'floppyv2');
+
 -- Fix the Epoch date
 UPDATE ITEM_VALUE SET START_DATE = '1970-01-01 00:00:00' WHERE START_DATE = '1970-01-01 01:00:00';
 
