c824793439ed9ed5e600fa2c007daa16ed37131a
Diggory Briercliffe <dig@amee.cc>
2010-04-22 16:25:13 +0100

    PL-1427 - Moved messaging code to separate library. Dependency hell. Other related changes.

--- server/amee-engine/src/main/java/com/amee/engine/spring/RestletRequestScope.java
+++ server/amee-engine/src/main/java/com/amee/engine/spring/RestletRequestScope.java
@@ -1,36 +1,36 @@
 package com.amee.engine.spring;
 
-import org.restlet.data.Request;
-import org.springframework.beans.factory.ObjectFactory;
-import org.springframework.beans.factory.config.Scope;
+public class RestletRequestScope {
 
-public class RestletRequestScope implements Scope {
-
-    public Object get(String name, ObjectFactory objectFactory) {
-        Object scopedObject = Request.getCurrent().getAttributes().get(name);
-        if (scopedObject == null) {
-            scopedObject = objectFactory.getObject();
-            Request.getCurrent().getAttributes().put(name, scopedObject);
-        }
-        return scopedObject;
-    }
-
-    public Object remove(String name) {
-        Object scopedObject = Request.getCurrent().getAttributes().get(name);
-        if (scopedObject != null) {
-            Request.getCurrent().getAttributes().remove(name);
-            return scopedObject;
-        } else {
-            return null;
-        }
-    }
-
-    public void registerDestructionCallback(String name, Runnable callback) {
-        // not supported
-    }
-
-    public String getConversationId() {
-        // not supported
-        return null;
-    }
 }
+//
+//public class RestletRequestScope implements Scope {
+//
+//    public Object get(String name, ObjectFactory objectFactory) {
+//        Object scopedObject = Request.getCurrent().getAttributes().get(name);
+//        if (scopedObject == null) {
+//            scopedObject = objectFactory.getObject();
+//            Request.getCurrent().getAttributes().put(name, scopedObject);
+//        }
+//        return scopedObject;
+//    }
+//
+//    public Object remove(String name) {
+//        Object scopedObject = Request.getCurrent().getAttributes().get(name);
+//        if (scopedObject != null) {
+//            Request.getCurrent().getAttributes().remove(name);
+//            return scopedObject;
+//        } else {
+//            return null;
+//        }
+//    }
+//
+//    public void registerDestructionCallback(String name, Runnable callback) {
+//        // not supported
+//    }
+//
+//    public String getConversationId() {
+//        // not supported
+//        return null;
+//    }
+//}
