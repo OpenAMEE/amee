3d065c583c9ec4c25905dcc1d431f63eb1cff07b
David Keen <david.keen@amee.com>
2010-05-05 16:12:37 +0100

    Fixed dodgy equals implementation. PL-1503.

--- server/amee-domain/src/main/java/com/amee/domain/path/PathItem.java
+++ server/amee-domain/src/main/java/com/amee/domain/path/PathItem.java
@@ -102,6 +102,14 @@ public class PathItem implements IAMEEEntityReference, APIObject, Comparable {
     }
 
     public boolean equals(Object o) {
+        if (o == this) {
+            return true;
+        }
+
+        if (!(o instanceof PathItem)) {
+            return false;
+        }
+        
         PathItem other = (PathItem) o;
         return getFullPath().equalsIgnoreCase(other.getFullPath());
     }
