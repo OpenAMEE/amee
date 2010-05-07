c7054a99c7bb91abbb8cf1a6c714fe0911f3598b
Diggory Briercliffe <dig@amee.cc>
2010-05-07 10:56:27 +0100

    PL-1506 - Refactoring of builders and introduction of renderers.

---
+++ server/amee-service/src/main/java/com/amee/platform/service/v3/item/DataItemBuilder.java
@@ -0,0 +1,125 @@
+package com.amee.platform.service.v3.item;
+
+import com.amee.base.resource.RequestWrapper;
+import com.amee.base.resource.ResourceBuilder;
+import com.amee.domain.data.DataCategory;
+import com.amee.domain.data.DataItem;
+import com.amee.domain.data.ItemDefinition;
+import com.amee.domain.path.PathItem;
+import com.amee.domain.path.PathItemGroup;
+import com.amee.service.data.DataService;
+import com.amee.service.environment.EnvironmentService;
+import com.amee.service.path.PathItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+public abstract class DataItemBuilder<E> implements ResourceBuilder<E> {
+
+    @Autowired
+    private EnvironmentService environmentService;
+
+    @Autowired
+    private DataService dataService;
+
+    @Autowired
+    private PathItemService pathItemService;
+
+    @Transactional(readOnly = true)
+    protected void handle(RequestWrapper requestWrapper, DataItemRenderer renderer) {
+        // Get DataCategory identifier.
+        String dataCategoryIdentifier = requestWrapper.getAttributes().get("categoryIdentifier");
+        if (dataCategoryIdentifier != null) {
+            // Get DataCategory.
+            DataCategory dataCategory = dataService.getDataCategoryByIdentifier(
+                    environmentService.getEnvironmentByName("AMEE"), dataCategoryIdentifier);
+            if (dataCategory != null) {
+                // Get DataItem identifier.
+                String dataItemIdentifier = requestWrapper.getAttributes().get("itemIdentifier");
+                if (dataItemIdentifier != null) {
+                    // Get DataItem.
+                    DataItem dataItem = dataService.getDataItemByUid(dataCategory, dataItemIdentifier);
+                    if (dataItem != null) {
+                        // Handle the DataItem.
+                        this.handle(requestWrapper, dataItem, renderer);
+                        renderer.ok();
+                    } else {
+                        renderer.notFound();
+                    }
+                } else {
+                    renderer.itemIdentifierMissing();
+                }
+            } else {
+                renderer.notFound();
+            }
+        } else {
+            renderer.categoryIdentifierMissing();
+        }
+    }
+
+    protected void handle(
+            RequestWrapper requestWrapper,
+            DataItem dataItem,
+            DataItemRenderer renderer) {
+
+        boolean full = requestWrapper.getMatrixParameters().containsKey("full");
+        boolean name = requestWrapper.getMatrixParameters().containsKey("name");
+        boolean path = requestWrapper.getMatrixParameters().containsKey("path");
+        boolean audit = requestWrapper.getMatrixParameters().containsKey("audit");
+        boolean wikiDoc = requestWrapper.getMatrixParameters().containsKey("wikiDoc");
+        boolean provenance = requestWrapper.getMatrixParameters().containsKey("provenance");
+        boolean itemDefinition = requestWrapper.getMatrixParameters().containsKey("itemDefinition");
+
+        // New DataItem & basic.
+        renderer.newDataItem(dataItem);
+        renderer.addBasic();
+
+        // Optionals.
+        if (name || full) {
+            renderer.addName();
+        }
+        if (path || full) {
+            PathItemGroup pathItemGroup = pathItemService.getPathItemGroup(dataItem.getEnvironment());
+            renderer.addPath(pathItemGroup.findByUId(dataItem.getDataCategory().getUid()));
+        }
+        if (audit || full) {
+            renderer.addAudit();
+        }
+        if (wikiDoc || full) {
+            renderer.addWikiDoc();
+        }
+        if (provenance || full) {
+            renderer.addProvenance();
+        }
+        if ((itemDefinition || full) && (dataItem.getItemDefinition() != null)) {
+            ItemDefinition id = dataItem.getItemDefinition();
+            renderer.addItemDefinition(id);
+        }
+    }
+
+    public interface DataItemRenderer {
+
+        public void ok();
+
+        public void notFound();
+
+        public void itemIdentifierMissing();
+
+        public void categoryIdentifierMissing();
+
+        public void newDataItem(DataItem dataItem);
+
+        public void addBasic();
+
+        public void addName();
+
+        public void addPath(PathItem pathItem);
+
+        public void addAudit();
+
+        public void addWikiDoc();
+
+        public void addProvenance();
+
+        public void addItemDefinition(ItemDefinition id);
+    }
+}
--- server/amee-service/src/main/java/com/amee/platform/service/v3/item/DataItemJSONBuilder.java
+++ server/amee-service/src/main/java/com/amee/platform/service/v3/item/DataItemJSONBuilder.java
@@ -1,128 +1,113 @@
 package com.amee.platform.service.v3.item;
 
 import com.amee.base.resource.RequestWrapper;
-import com.amee.base.resource.ResourceBuilder;
-import com.amee.domain.data.DataCategory;
 import com.amee.domain.data.DataItem;
 import com.amee.domain.data.ItemDefinition;
 import com.amee.domain.path.PathItem;
-import com.amee.domain.path.PathItemGroup;
-import com.amee.service.data.DataService;
-import com.amee.service.environment.EnvironmentService;
-import com.amee.service.path.PathItemService;
 import org.joda.time.format.DateTimeFormatter;
 import org.joda.time.format.ISODateTimeFormat;
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Scope("prototype")
-public class DataItemJSONBuilder implements ResourceBuilder<JSONObject> {
+public class DataItemJSONBuilder extends DataItemBuilder<JSONObject> {
 
     private final static DateTimeFormatter FMT = ISODateTimeFormat.dateTimeNoMillis();
 
-    @Autowired
-    private EnvironmentService environmentService;
-
-    @Autowired
-    private DataService dataService;
-
-    @Autowired
-    private PathItemService pathItemService;
-
-    @Transactional(readOnly = true)
     public JSONObject handle(RequestWrapper requestWrapper) {
-        try {
-            JSONObject o = new JSONObject();
-            // Get DataCategory identifier.
-            String categoryIdentifier = requestWrapper.getAttributes().get("categoryIdentifier");
-            if (categoryIdentifier != null) {
-                // Get DataCategory.
-                DataCategory dataCategory = dataService.getDataCategoryByIdentifier(
-                        environmentService.getEnvironmentByName("AMEE"), categoryIdentifier);
-                if (dataCategory != null) {
-                    // Get DataItem identifier.
-                    String dataItemIdentifier = requestWrapper.getAttributes().get("itemIdentifier");
-                    if (dataItemIdentifier != null) {
-                        // Get DataItem.
-                        DataItem dataItem = dataService.getDataItemByUid(dataCategory, dataItemIdentifier);
-                        if (dataItem != null) {
-                            // Get DataItem Element.
-                            o.put("item", getDataItemJSONObject(requestWrapper, dataItem));
-                            o.put("status", "OK");
-                        } else {
-                            o.put("status", "NOT_FOUND");
-                        }
-                    } else {
-                        o.put("status", "ERROR");
-                        o.put("error", "The itemIdentifier was missing.");
-                    }
-                } else {
-                    o.put("status", "NOT_FOUND");
-                }
-            } else {
-                o.put("status", "ERROR");
-                o.put("error", "The categoryIdentifier was missing.");
-            }
-            return o;
-        } catch (Exception e) {
-            throw new RuntimeException("Caught JSONException: " + e.getMessage(), e);
-        }
-    }
-
-    protected JSONObject getDataItemJSONObject(RequestWrapper requestWrapper, DataItem dataItem) throws JSONException {
-
-        JSONObject itemObj = new JSONObject();
-        boolean full = requestWrapper.getMatrixParameters().containsKey("full");
-        boolean name = requestWrapper.getMatrixParameters().containsKey("name");
-        boolean path = requestWrapper.getMatrixParameters().containsKey("path");
-        boolean audit = requestWrapper.getMatrixParameters().containsKey("audit");
-        boolean wikiDoc = requestWrapper.getMatrixParameters().containsKey("wikiDoc");
-        boolean provenance = requestWrapper.getMatrixParameters().containsKey("provenance");
-        boolean itemDefinition = requestWrapper.getMatrixParameters().containsKey("itemDefinition");
-
-        // Basic attributes.
-        itemObj.put("uid", dataItem.getUid());
-
-        // Optional attributes.
-        if (name || full) {
-            itemObj.put("name", dataItem.getName());
-            itemObj.put("categoryWikiName", dataItem.getDataCategory().getWikiName());
-        }
-        if (path || full) {
-            // Get PathItem.
-            PathItemGroup pathItemGroup = pathItemService.getPathItemGroup(dataItem.getEnvironment());
-            PathItem pathItem = pathItemGroup.findByUId(dataItem.getDataCategory().getUid());
-            // Add Paths.
-            itemObj.put("path", dataItem.getPath());
-            if (pathItem != null) {
-                itemObj.put("fullPath", pathItem.getFullPath() + "/" + dataItem.getDisplayPath());
-            }
-        }
-        if (audit || full) {
-            itemObj.put("status", dataItem.getStatus().getName());
-            itemObj.put("created", FMT.print(dataItem.getCreated().getTime()));
-            itemObj.put("modified", FMT.print(dataItem.getModified().getTime()));
-        }
-        if (wikiDoc || full) {
-            itemObj.put("wikiDoc", dataItem.getWikiDoc());
-        }
-        if (provenance || full) {
-            itemObj.put("provenance", dataItem.getProvenance());
-        }
-        if ((itemDefinition || full) && (dataItem.getItemDefinition() != null)) {
-            ItemDefinition id = dataItem.getItemDefinition();
-            itemObj.put("itemDefinition", new JSONObject().put("uid", id.getUid()).put("name", id.getName()));
-        }
-
-        return itemObj;
+        DataItemJSONRenderer renderer = new DataItemJSONRenderer();
+        super.handle(requestWrapper, renderer);
+        return renderer.getJSONObject();
     }
 
     public String getMediaType() {
         return "application/json";
+    }
+
+    public class DataItemJSONRenderer implements DataItemBuilder.DataItemRenderer {
+
+        private DataItem dataItem;
+        private JSONObject rootObj;
+        private JSONObject dataItemObj;
+
+        public DataItemJSONRenderer() {
+            super();
+            rootObj = new JSONObject();
+        }
+
+        public void ok() {
+            put(rootObj, "status", "OK");
+        }
+
+        public void notFound() {
+            put(rootObj, "status", "NOT_FOUND");
+        }
+
+        public void itemIdentifierMissing() {
+            put(rootObj, "status", "ERROR");
+            put(rootObj, "error", "The itemIdentifier was missing.");
+        }
+
+        public void categoryIdentifierMissing() {
+            put(rootObj, "status", "ERROR");
+            put(rootObj, "error", "The categoryIdentifier was missing.");
+        }
+
+        public void newDataItem(DataItem dataItem) {
+            this.dataItem = dataItem;
+            dataItemObj = new JSONObject();
+        }
+
+        public void addBasic() {
+            put(dataItemObj, "uid", dataItem.getUid());
+        }
+
+        public void addName() {
+            put(dataItemObj, "name", dataItem.getName());
+            put(dataItemObj, "categoryWikiName", dataItem.getDataCategory().getWikiName());
+        }
+
+        public void addPath(PathItem pathItem) {
+            put(dataItemObj, "path", dataItem.getPath());
+            if (pathItem != null) {
+                put(dataItemObj, "fullPath", pathItem.getFullPath() + "/" + dataItem.getDisplayPath());
+            }
+        }
+
+        public void addAudit() {
+            put(dataItemObj, "status", dataItem.getStatus().getName());
+            put(dataItemObj, "created", FMT.print(dataItem.getCreated().getTime()));
+            put(dataItemObj, "modified", FMT.print(dataItem.getModified().getTime()));
+        }
+
+        public void addWikiDoc() {
+            put(dataItemObj, "wikiDoc", dataItem.getWikiDoc());
+        }
+
+        public void addProvenance() {
+            put(dataItemObj, "provenance", dataItem.getProvenance());
+        }
+
+        public void addItemDefinition(ItemDefinition itemDefinition) {
+            JSONObject itemDefinitionObj = new JSONObject();
+            put(itemDefinitionObj, "uid", itemDefinition.getUid());
+            put(itemDefinitionObj, "name", itemDefinition.getName());
+            put(dataItemObj, "itemDefinition", itemDefinitionObj);
+        }
+
+        protected JSONObject put(JSONObject o, String key, Object value) {
+            try {
+                return o.put(key, value);
+            } catch (JSONException e) {
+                throw new RuntimeException("Caught JSONException: " + e.getMessage(), e);
+            }
+        }
+
+        public JSONObject getJSONObject() {
+            return rootObj;
+        }
     }
 }
