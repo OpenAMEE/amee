ffaff739e70d27333bf28a7279f366813ada5457
Diggory Briercliffe <dig@amee.cc>
2010-05-11 10:12:18 +0100

    PL-1548 - Enhancements to Data Category filtering.

---
+++ server/amee-service/src/main/java/com/amee/platform/search/DataCategoryFilter.java
@@ -0,0 +1,127 @@
+package com.amee.platform.search;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.KeywordAnalyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.util.Version;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
+public class DataCategoryFilter implements Serializable {
+
+    public final static Analyzer STANDARD_ANALYZER = new StandardAnalyzer(Version.LUCENE_30);
+    public final static Analyzer KEYWORD_ANALYZER = new KeywordAnalyzer();
+
+    private Map<String, Query> queries = new HashMap<String, Query>();
+
+    public DataCategoryFilter() {
+        super();
+    }
+
+    public Query getUid() {
+        return getQueries().get("uid");
+    }
+
+    public void setUid(Query uid) {
+        getQueries().put("uid", uid);
+    }
+
+    public Query getName() {
+        return getQueries().get("name");
+    }
+
+    public void setName(Query name) {
+        getQueries().put("name", name);
+    }
+
+    public Query getPath() {
+        return getQueries().get("path");
+    }
+
+    public void setPath(Query path) {
+        getQueries().put("path", path);
+    }
+
+    public Query getFullPath() {
+        return getQueries().get("fullPath");
+    }
+
+    public void setFullPath(Query fullPath) {
+        getQueries().put("fullPath", fullPath);
+    }
+
+    public Query getWikiName() {
+        return getQueries().get("wikiName");
+    }
+
+    public void setWikiName(Query wikiName) {
+        getQueries().put("wikiName", wikiName);
+    }
+
+    public Query getWikiDoc() {
+        return getQueries().get("wikiDoc");
+    }
+
+    public void setWikiDoc(Query wikiDoc) {
+        getQueries().put("wikiDoc", wikiDoc);
+    }
+
+    public Query getProvenance() {
+        return getQueries().get("provenance");
+    }
+
+    public void setProvenance(Query provenance) {
+        getQueries().put("provenance", provenance);
+    }
+
+    public Query getAuthority() {
+        return getQueries().get("authority");
+    }
+
+    public void setAuthority(Query authority) {
+        getQueries().put("authority", authority);
+    }
+
+    public Query getParentUid() {
+        return getQueries().get("parentUid");
+    }
+
+    public void setParentUid(Query parentUid) {
+        getQueries().put("parentUid", parentUid);
+    }
+
+    public Query getParentWikiName() {
+        return getQueries().get("parentWikiName");
+    }
+
+    public void setParentWikiName(Query parentWikiName) {
+        getQueries().put("parentWikiName", parentWikiName);
+    }
+
+    public Query getItemDefinitionUid() {
+        return getQueries().get("itemDefinitionUid");
+    }
+
+    public void setItemDefinitionUid(Query itemDefinitionUid) {
+        getQueries().put("itemDefinitionUid", itemDefinitionUid);
+    }
+
+    public Query getItemDefinitionName() {
+        return getQueries().get("itemDefinitionName");
+    }
+
+    public void setItemDefinitionName(Query itemDefinitionName) {
+        getQueries().put("itemDefinitionName", itemDefinitionName);
+    }
+
+    public Map<String, Query> getQueries() {
+        return queries;
+    }
+
+    public void setQueries(Map<String, Query> queries) {
+        this.queries = queries;
+    }
+}
--- server/amee-service/src/main/java/com/amee/platform/service/v3/category/DataCategoriesJSONBuilder.java
+++ server/amee-service/src/main/java/com/amee/platform/service/v3/category/DataCategoriesJSONBuilder.java
@@ -2,7 +2,10 @@ package com.amee.platform.service.v3.category;
 
 import com.amee.base.resource.RequestWrapper;
 import com.amee.base.resource.ResourceBuilder;
+import com.amee.base.validation.ValidationException;
 import com.amee.domain.data.DataCategory;
+import com.amee.platform.search.DataCategoryFilter;
+import com.amee.platform.search.DataCategoryFilterValidationHelper;
 import com.amee.platform.search.SearchService;
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -17,26 +20,35 @@ import org.springframework.transaction.annotation.Transactional;
 public class DataCategoriesJSONBuilder implements ResourceBuilder<JSONObject> {
 
     @Autowired
+    private SearchService searchService;
+
+    @Autowired
     private DataCategoryJSONBuilder dataCategoryJSONBuilder;
 
     @Autowired
-    private SearchService searchService;
+    private DataCategoryFilterValidationHelper validationHelper;
 
     @Transactional(readOnly = true)
     public JSONObject handle(RequestWrapper requestWrapper) {
         try {
             JSONObject o = new JSONObject();
-            o.put("categories", getDataCategoriesJSONArray(requestWrapper));
-            o.put("status", "OK");
+            DataCategoryFilter filter = new DataCategoryFilter();
+            validationHelper.setDataCategoryFilter(filter);
+            if (validationHelper.isValid(requestWrapper.getQueryParameters())) {
+                o.put("categories", getDataCategoriesJSONArray(requestWrapper, filter));
+                o.put("status", "OK");
+            } else {
+                throw new ValidationException(validationHelper.getValidationResult());
+            }
             return o;
         } catch (JSONException e) {
             throw new RuntimeException("Caught JSONException: " + e.getMessage(), e);
         }
     }
 
-    protected JSONArray getDataCategoriesJSONArray(RequestWrapper requestWrapper) throws JSONException {
+    protected JSONArray getDataCategoriesJSONArray(RequestWrapper requestWrapper, DataCategoryFilter filter) throws JSONException {
         JSONArray categories = new JSONArray();
-        for (DataCategory dataCategory : searchService.getDataCategories(requestWrapper)) {
+        for (DataCategory dataCategory : searchService.getDataCategories(filter)) {
             categories.put(dataCategoryJSONBuilder.getDataCategoryJSONObject(requestWrapper, dataCategory));
         }
         return categories;
