134a5d24b2e53c42a9190462404c7fecb6ca94d9
David Keen <david.keen@amee.com>
2010-05-12 14:35:54 +0100

    Capistrano local deploy. OPS-63.

--- deploy/local.rb
+++ deploy/local.rb
@@ -2,14 +2,19 @@
 role :app, "localhost"
 role :db,  "localhost", :primary => true
 
-set :user, "david.keen"
-set :scm_command, "/opt/local/bin/git"
+unset :user
+#set :user, "local.user"
 
-set :application, "amee-test"
+set :application, "amee"
 set :deploy_to, "/var/www/apps/#{application}"
 
-# Don't deploy from github
+# Deploy from local folder
 set :repository,  "/Development/AMEE/amee.deploy"
-#set :deploy_via, :copy
-#set :copy_cache, true
-#set :copy_exclude, [".git"]
+
+#set :scm, :git
+#set :scm_command, "/opt/local/bin/git"
+
+set :scm, :none
+set :deploy_via, :copy
+set :copy_exclude, [".git"]
+
