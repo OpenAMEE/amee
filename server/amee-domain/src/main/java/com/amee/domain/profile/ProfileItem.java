bd57d3537d21f1b8614882b15bc4f2abe2557857
David Keen <david.keen@amee.com>
2010-05-13 10:31:03 +0100

    Reverted b21928cc4e9b2c44895fec6646219c30f583eb99 because we need to keep non-standard date formats for compatibility.

--- server/amee-domain/src/main/java/com/amee/domain/profile/ProfileItem.java
+++ server/amee-domain/src/main/java/com/amee/domain/profile/ProfileItem.java
@@ -147,7 +147,7 @@ public class ProfileItem extends Item {
     }
 
     public void setStartDate(Date startDate) {
-        this.startDate = new Date(startDate.getTime());
+        this.startDate = startDate;
     }
 
     public StartEndDate getEndDate() {
@@ -159,8 +159,7 @@ public class ProfileItem extends Item {
     }
 
     public void setEndDate(Date endDate) {
-        // May be null.
-        this.endDate = endDate != null ? new Date(endDate.getTime()) : null;
+        this.endDate = endDate;
     }
 
     public boolean isEnd() {
