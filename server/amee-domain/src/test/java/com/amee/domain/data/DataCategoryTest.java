a39b4833aa9739b7bb253810f46d332d614ad8a1
David Keen <david.keen@amee.com>
2010-05-19 11:46:16 +0100

    Trashing. PL-192.

--- server/amee-domain/src/test/java/com/amee/domain/data/DataCategoryTest.java
+++ server/amee-domain/src/test/java/com/amee/domain/data/DataCategoryTest.java
@@ -1,35 +1,60 @@
 package com.amee.domain.data;
 
 import com.amee.domain.AMEEStatus;
+import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 public class DataCategoryTest {
+    private DataCategory dc;
+    private DataCategory mockParentDc;
+    private ItemDefinition mockItemDef;
+
+
+    @Before
+    public void setUp() {
+
+        dc = new DataCategory();
+
+        mockItemDef = mock(ItemDefinition.class);
+        dc.setItemDefinition(mockItemDef);
+
+        mockParentDc = mock(DataCategory.class);
+        dc.setDataCategory(mockParentDc);
+    }
 
     @Test
-    public void testIsTrash() {
+    public void noneTrashed() {
 
         // A DataCategory should be considered trashed if:
-        // itself is trashed or its ItemDefinition is trashed.
+        // itself is trashed or its ItemDefinition is trashed or its parent DataCategory is trashed.
 
-        ItemDefinition itemDef = new ItemDefinition();
-        DataCategory dc = new DataCategory();
-        dc.setItemDefinition(itemDef);
-
-        // ACTIVE DC + ACTIVE ID
+        when(mockItemDef.isTrash()).thenReturn(false);
+        when(mockParentDc.isTrash()).thenReturn(false);
         assertFalse("DataCategory should be active", dc.isTrash());
 
-        // TRASHED DC + ACTIVE ID
+        verify(mockItemDef).isTrash();
+        verify(mockParentDc).isTrash();
+    }
+
+    @Test
+    public void itselfTrashed() {
         dc.setStatus(AMEEStatus.TRASH);
         assertTrue("DataCategory is trashed", dc.isTrash());
+    }
 
-        // ACTIVE DC + TRASHED ID
-        dc.setStatus(AMEEStatus.ACTIVE);
-        itemDef.setStatus(AMEEStatus.TRASH);
+    @Test
+    public void parentTrashed() {
+        when(mockParentDc.isTrash()).thenReturn(true);
+        assertTrue("DataCategory should be trashed", dc.isTrash());
+        verify(mockParentDc).isTrash();
+    }
+
+    @Test
+    public void itemDefTrashed() {
+        when(mockItemDef.isTrash()).thenReturn(true);
         assertTrue("Linked ItemDefinition is trashed", dc.isTrash());
-
-        // TRASHED DC + TRASHED ID
-        dc.setStatus(AMEEStatus.TRASH);
-        assertTrue("DataCategory is trashed", dc.isTrash());
+        verify(mockItemDef).isTrash();
     }
 }
