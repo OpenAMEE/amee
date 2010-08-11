cf116f87ffb2b3b933c8e45850becbbb14f946cd
Diggory Briercliffe <dig@amee.cc>
2010-08-11 14:22:38 +0100

    INFRA-294 - WIP update to generate.groovy.

--- scripts/groovy/generate.groovy
+++ scripts/groovy/generate.groovy
@@ -8,16 +8,15 @@ import groovy.sql.Sql
 // * itemCount = 500
 
 // Testing scale (see above).
-def sub1Count = 1
-def sub2Count = 1
-def valueDefCount = 5
-def itemCount = 5
-def itemValueBatch = 300
-def itemValueBatchGroup = itemValueBatch * 10
-def itemValuesExpected = sub1Count * sub2Count * itemCount * valueDefCount;
+levelOneCategoryCount = 1
+levelTwoCategoryCount = 16
+valueDefCount = 15
+itemCount = 500
+itemValueBatch = 1000
+itemValuesExpected = levelOneCategoryCount * levelTwoCategoryCount * itemCount * valueDefCount;
 
 // The environmentId is always 2.
-def environmentId = 2
+environmentId = 2
 
 // Add a random character generator to the String class.
 addRandomStringMethodToString()
@@ -41,195 +40,185 @@ def password = "amee"
 if (opt.p) password = opt.p
 
 // Configure DataSource.
-def sql = Sql.newInstance("jdbc:mysql://${server}:3306/${database}", user, password, "com.mysql.jdbc.Driver")
+sql = Sql.newInstance("jdbc:mysql://${server}:3306/${database}", user, password, "com.mysql.jdbc.Driver")
 sql.connection.autoCommit = false
 
-// 1) Find the Root & LCA data categories
+// Item Value batch vars.
+itemValueSql = "INSERT INTO ITEM_VALUE (UID, STATUS, CREATED, MODIFIED, ITEM_VALUE_DEFINITION_ID, ITEM_ID, VALUE, START_DATE) " +
+  "VALUES (?, 1, NOW(), NOW(), ?, ?, ?, ?)";
+itemValueStatement = sql.connection.prepareStatement(itemValueSql)
+batchCount = 0
+
+// Find the Root & LCA data categories
 def rootCategoryId = getRootCategoryId()
-def rootLCACategoryId = getOrCreateRootLCACategory(environmentId, rootCategoryId)
+def rootLCACategoryId = getOrCreateRootLCACategory(rootCategoryId)
 println "Top level LCA DATA_CATEGORY '${rootLCACategoryId}'."
-
-// 2) Create sub1Count sub1 DCs with parent ID created in (1)
-sub1Keys = getLevelOneSubCategoryIDs(environmentId, rootLCACategoryId);
-
-// 3) Create Data Categories and Item Definitions.
-dataCategoryToItemDefinitionMap = getDataCategoryToItemDefinitionMap(environmentId, sub1Keys);
-
-// 5) For each sub2 ItemDef created in (3) create itemCount Items
-itemKeys = getItemIDs(dataCategoryToItemDefinitionMap);
 
 // Commit all so far.
 sql.commit()
 
+// Create sub1Count sub1 DCs with parent ID created in (1)
+def levelOneCategoryIds = createLevelOneCategories(rootLCACategoryId);
 
-println "Creating ${itemValuesExpected} Item Values."
+// Commit all so far.
+sql.commit()
 
-// SQL to create ItemValues.
-ivSql = "INSERT INTO ITEM_VALUE (UID, STATUS, CREATED, MODIFIED, ITEM_VALUE_DEFINITION_ID, ITEM_ID, VALUE, START_DATE) " +
-  "VALUES (?, 1, NOW(), NOW(), ?, ?, ?, ?)";
-stmt = sql.connection.prepareStatement(ivSql)
+// Create Data Categories and Item Definitions.
+createLevelTwoDataCategories(levelOneCategoryIds);
 
-// Counter for batching.
-batchCount = 0
-batchGroupCount = 0
-
-// 6) For each Item created in (5)
-//    For each ItemValueDef for that Item
-//      create an ItemValue
-itemKeys.each { itemKey ->
-
-  // TODO: Use value types (numbers, strings) that match the IVD & VD.
-  // Get a list of Item Value Definitions for this item's item definition
-
-  sql.eachRow("SELECT DISTINCT ITEM_VALUE_DEFINITION.ID FROM ITEM_VALUE_DEFINITION " +
-      "JOIN ITEM_DEFINITION ON (ITEM_VALUE_DEFINITION.ITEM_DEFINITION_ID = ITEM_DEFINITION.ID) " +
-      "JOIN ITEM ON (ITEM.ITEM_DEFINITION_ID = ITEM_DEFINITION.ID) " +
-      "WHERE ITEM.ID = ${itemKey[0][0]}") { row ->
-
-    // Vars for this ItemValue.
-    itemValueDefinitionId = row.ID
-    itemId = itemKey[0][0]
-    value = String.randomString(5)
-
-    // Create a single batch entry.
-    stmt.setObject(1, UidGen.INSTANCE_12.getUid())
-    stmt.setObject(2, itemValueDefinitionId)
-    stmt.setObject(3, itemId)
-    stmt.setObject(4, value)
-    stmt.setObject(5, "1970-01-01 00:00:00")
-    stmt.addBatch()
-
-    // Handle batch.
-    batchCount++
-    batchGroupCount++
-    if (batchCount >= itemValueBatch) {
-      // Execute this batch.
-      stmt.executeBatch();
-      println "Created ${batchCount} ITEM_VALUEs in a batch.";
-      batchCount = 0;
-      // End of batch group? If so we can commit.
-      if (batchGroupCount > itemValueBatchGroup) {
-        sql.commit()
-        println "Starting new batch group after ${batchGroupCount} ITEM_VALUEs.";
-        batchGroupCount = 0;
-      }
-      // Start new statement for next batch.
-      stmt.close()
-      stmt = sql.connection.prepareStatement(ivSql)
-    }
-  }
-}
-
-// Handle remaining Item Values in current batch.
-if (batchCount > 0) {
-  stmt.executeBatch()
-  sql.commit()
-  println "Created ${batchCount} ITEM_VALUEs in a batch.";
-}
-stmt.close()
-
-println "Created all ITEM_VALUEs.";
+println "Created everything.";
 
 // ***** Private methods below. *****
 
 // 2) Create sub1Count sub1 DCs with parent ID created in (1)
-private List getLevelOneSubCategoryIDs(environmentId, rootLCACategoryId) {
-  println "Creating ${sub1Count} DATA_CATEGORYs with parent ID: ${rootLCACategoryId}."
-  sub1Keys = []
-  sub1Count.times {
-    name = 'LCA_' + String.randomString(12)
-    path = name
-    dataCategoryId = rootLCACategoryId
-    wikiName = name
-    sub1Keys << sql.executeInsert(
+private List createLevelOneCategories(rootLCACategoryId) {
+  println "Creating ${levelOneCategoryCount} DATA_CATEGORYs with parent ID: ${rootLCACategoryId}."
+  def levelOneCategoryIds = []
+  levelOneCategoryCount.times {
+    def name = 'LCA_' + String.randomString(12)
+    def path = name
+    def dataCategoryId = rootLCACategoryId
+    def wikiName = name
+    def levelOneCategoryKey = sql.executeInsert(
             "INSERT INTO DATA_CATEGORY (UID, STATUS, NAME, PATH, CREATED, MODIFIED, ENVIRONMENT_ID, DATA_CATEGORY_ID, WIKI_NAME) " +
                     "VALUES (?, 1, ?, ?, NOW(), NOW(), ?, ?, ?)",
             [UidGen.INSTANCE_12.getUid(), name, path, environmentId, dataCategoryId, wikiName])
+    levelOneCategoryIds << levelOneCategoryKey[0][0]
   }
-  return sub1Keys
+  return levelOneCategoryIds
 }
 
 // 3) For each sub1 DC created in (2)
 //    create sub2Count DCs with ItemDefs
 //    store the list of ItemDef IDs and DC IDs created in a Map(DC_ID => ItemDef_ID)
-private def getDataCategoryToItemDefinitionMap(environmentId, sub1Keys) {
+private def createLevelTwoDataCategories(levelOneCategoryIds) {
 
-  dataCategoryToItemDefinitionMap = [:]
+  levelOneCategoryIds.each { levelOneCategoryId ->
 
-  sub1Keys.each { sub1Key ->
-
-    sub2Keys = []
-    sub2Count.times {
-
-      // Create an Item Def
-      name = 'LCA_' + String.randomString(12)
+    levelTwoCategoryCount.times {
 
       // TODO: Typically drill downs only cover 2 or 3 IVDs rather than all of them.
       // Create drill down list.
       // Underscore used here is IVD paths need to be JS compatible.
-      drillDownList = []
+      def drillDownList = []
       valueDefCount.times {
         drillDownList << 'LCA_' + String.randomString(4)
       }
 
-      itemDefKeys = sql.executeInsert("INSERT INTO ITEM_DEFINITION (UID, STATUS, NAME, DRILL_DOWN, CREATED, MODIFIED, ENVIRONMENT_ID)" +
-              "VALUES (?, 1, ?, ?, NOW(), NOW(), ?)",
-              [UidGen.INSTANCE_12.getUid(), name, drillDownList.join(','), environmentId])
+      // Create Item Definition.
+      def itemDefinitionId = createItemDefinition('LCA_' + String.randomString(12), drillDownList)
 
-      itemDefKey = itemDefKeys[0][0]
-      println "Created ITEM_DEFINITION '${itemDefKey}'."
+      // Create Level Two Data Category.
+      def levelTwoCategoryId = createLevelTwoDataCategory(levelOneCategoryId, itemDefinitionId)
 
-      // And a linked Data Category
-      name = 'LCA_' + String.randomString(12)
-      path = name
-      dataCategoryId = sub1Key[0][0]
-      wikiName = name
-      sub2Keys = sql.executeInsert("INSERT INTO DATA_CATEGORY (UID, STATUS, NAME, PATH, CREATED, MODIFIED, ENVIRONMENT_ID, DATA_CATEGORY_ID, ITEM_DEFINITION_ID, WIKI_NAME)" +
-              "VALUES (?, 1, ?, ?, NOW(), NOW(), ?, ?, ?, ?)",
-              [UidGen.INSTANCE_12.getUid(), name, path, environmentId, dataCategoryId, itemDefKey, wikiName])
+      // For each sub2 ItemDef created in (3) create itemCount Items
+      def itemIds = createDataItems(levelTwoCategoryId, itemDefinitionId);
 
-      println "Created DATA_CATEGORY '${sub2Keys[0][0]}' with parent DATA_CATEGORY_ID '${dataCategoryId}' and ITEM_DEFINITION_ID '${itemDefKey}'."
+      // Create Item Value Definitions
+      def itemValueDefinitionIds = createItemValueDefinitions(itemDefinitionId, drillDownList)
 
-      // Map of DC IDs to Item Def IDs
-      dataCategoryToItemDefinitionMap[(sub2Keys[0][0])] = itemDefKeys[0][0]
+      // Commit all so far.
+      sql.commit()
 
-      // TODO: Use workable Value Definition IDs.
-      // 4) For each sub2 ItemDef created in (3)
-      //   create valueDefCount ItemValueDefs that match the drill downs set in the Item Def
-
-      valueDefKeys = []
-      drillDownList.each { drillDown ->
-        name = drillDown
-        path = drillDown
-        itemDefinitionId = itemDefKeys[0][0]
-        rand = new Random()
-        valueDefinitionId = rand.nextInt(100)
-        valueDefKey = sql.executeInsert("INSERT INTO ITEM_VALUE_DEFINITION (UID, STATUS, NAME, PATH, FROM_PROFILE, FROM_DATA, CREATED, MODIFIED, ENVIRONMENT_ID, ITEM_DEFINITION_ID, VALUE_DEFINITION_ID, ALLOWED_ROLES)" +
-                "VALUES (?, 1, ?, ?, ?, ?, NOW(), NOW(), ?, ?, ?, '')",
-                [UidGen.INSTANCE_12.getUid(), name, path, 0, 1, environmentId, itemDefinitionId, valueDefinitionId])
-        valueDefKeys << valueDefKey
-        println "Created ITEM_VALUE_DEFINITION '${valueDefKey[0][0]}' with NAME '${drillDown}'."
+      // For each Data Item created in:
+      //   For each ItemValueDef for that Data Item:
+      //      create an Item Value:
+      itemIds.each { itemId ->
+        createItemValues(itemValueDefinitionIds, itemId)
       }
+
+      // Handle remaining Item Values in current batch.
+      if (batchCount > 0) {
+        itemValueStatement.executeBatch()
+        println "Created ${batchCount} ITEM_VALUEs in a batch.";
+        batchCount = 0
+      }
+
+      // Commit all so far.
+      sql.commit()
     }
   }
-
-  return dataCategoryToItemDefinitionMap
 }
 
-// 5) For each sub2 ItemDef created in (3) create itemCount Items
-private List getItemIDs(dataCategoryToItemDefinitionMap) {
-  itemKeys = []
-  dataCategoryToItemDefinitionMap.each { catId, itemDefId ->
-    itemCount.times {
-      itemKey = sql.executeInsert(
-              "INSERT INTO ITEM (UID, STATUS, CREATED, MODIFIED, ENVIRONMENT_ID, ITEM_DEFINITION_ID, DATA_CATEGORY_ID, TYPE, NAME, PATH) " +
-                      "VALUES (?, 1, NOW(), NOW(), ?, ?, ?, ?, ?, '')",
-              [UidGen.INSTANCE_12.getUid(), environmentId, itemDefId, catId, 'DI', 'LCA data item'])
-      itemKeys << itemKey
+// Create Item Values in batches.
+private def createItemValues(itemValueDefinitionIds, itemId) {
+
+  // Iterate over all Item Value Definitions.
+  itemValueDefinitionIds.each { itemValueDefinitionId ->
+
+    // Create a single batch entry.
+    itemValueStatement.setObject(1, UidGen.INSTANCE_12.getUid())
+    itemValueStatement.setObject(2, itemValueDefinitionId)
+    itemValueStatement.setObject(3, itemId)
+    itemValueStatement.setObject(4, String.randomString(5))
+    itemValueStatement.setObject(5, "1970-01-01 00:00:00")
+    itemValueStatement.addBatch()
+
+    // Handle batch.
+    batchCount++
+    if (batchCount >= itemValueBatch) {
+      // Execute this batch.
+      itemValueStatement.executeBatch();
+      println "Created ${batchCount} ITEM_VALUEs in a batch.";
+      batchCount = 0;
     }
-    println "Created $itemCount ITEMs for DATA_CATEGORY_ID '${catId}' and ITEM_DEFINITION_ID '${itemDefId}'."
   }
-  return itemKeys;
+}
+
+// Create Data Category
+private def createLevelTwoDataCategory(levelOneCategoryId, itemDefinitionId) {
+  def name = 'LCA_' + String.randomString(12)
+  def path = name
+  def wikiName = name
+  def levelTwoCategoryKey = sql.executeInsert("INSERT INTO DATA_CATEGORY (UID, STATUS, NAME, PATH, CREATED, MODIFIED, ENVIRONMENT_ID, DATA_CATEGORY_ID, ITEM_DEFINITION_ID, WIKI_NAME)" +
+          "VALUES (?, 1, ?, ?, NOW(), NOW(), ?, ?, ?, ?)",
+          [UidGen.INSTANCE_12.getUid(), name, path, environmentId, levelOneCategoryId, itemDefinitionId, wikiName])
+  def levelTwoCategoryId = levelTwoCategoryKey[0][0]
+  println "Created DATA_CATEGORY '${levelTwoCategoryId}' with parent DATA_CATEGORY_ID '${levelOneCategoryId}' and ITEM_DEFINITION_ID '${itemDefinitionId}'."
+  return levelTwoCategoryId;
+}
+
+// 4) For each sub2 ItemDef created in (3)
+//   create valueDefCount ItemValueDefs that match the drill downs set in the Item Def
+private def createItemValueDefinitions(itemDefinitionId, List drillDownList) {
+  def itemValueDefinitionIds = []
+  drillDownList.each { drillDown ->
+    def name = drillDown
+    def path = drillDown
+    def rand = new Random()
+    def valueDefinitionId = rand.nextInt(100)
+    def itemValueDefinitionKey = sql.executeInsert("INSERT INTO ITEM_VALUE_DEFINITION (UID, STATUS, NAME, PATH, FROM_PROFILE, FROM_DATA, CREATED, MODIFIED, ENVIRONMENT_ID, ITEM_DEFINITION_ID, VALUE_DEFINITION_ID, ALLOWED_ROLES)" +
+            "VALUES (?, 1, ?, ?, ?, ?, NOW(), NOW(), ?, ?, ?, '')",
+            [UidGen.INSTANCE_12.getUid(), name, path, 0, 1, environmentId, itemDefinitionId, valueDefinitionId])
+    def itemValueDefinitionId = itemValueDefinitionKey[0][0]
+    println "Created ITEM_VALUE_DEFINITION '${itemValueDefinitionId}' with NAME '${drillDown}'."
+    itemValueDefinitionIds << itemValueDefinitionId
+  }
+  return itemValueDefinitionIds
+}
+
+// Create Item Definition.
+private def createItemDefinition(String name, List drillDownList) {
+  def itemDefinitionKey = sql.executeInsert("INSERT INTO ITEM_DEFINITION (UID, STATUS, NAME, DRILL_DOWN, CREATED, MODIFIED, ENVIRONMENT_ID)" +
+          "VALUES (?, 1, ?, ?, NOW(), NOW(), ?)",
+          [UidGen.INSTANCE_12.getUid(), name, drillDownList.join(','), environmentId])
+  def itemDefinitionId = itemDefinitionKey[0][0]
+  println "Created ITEM_DEFINITION '${itemDefinitionId}'."
+  return itemDefinitionId;
+}
+
+// For each sub2 ItemDef created in (3) create itemCount Items
+private List createDataItems(dataCategoryId, itemDefinitionId) {
+  itemIds = []
+  itemCount.times {
+    itemKey = sql.executeInsert(
+            "INSERT INTO ITEM (UID, STATUS, CREATED, MODIFIED, ENVIRONMENT_ID, ITEM_DEFINITION_ID, DATA_CATEGORY_ID, TYPE, NAME, PATH) " +
+                    "VALUES (?, 1, NOW(), NOW(), ?, ?, ?, ?, ?, '')",
+            [UidGen.INSTANCE_12.getUid(), environmentId, itemDefinitionId, dataCategoryId, 'DI', 'LCA data item'])
+    itemIds << itemKey[0][0]
+  }
+  println "Created $itemCount ITEMs for DATA_CATEGORY_ID '${dataCategoryId}' and ITEM_DEFINITION_ID '${itemDefinitionId}'."
+  return itemIds;
 }
 
 private def configureCliBuilder() {
@@ -261,7 +250,7 @@ private def getRootCategoryId() {
 }
 
 // 1) Get or create the single top data category for LCA data called 'lca'.
-private def getOrCreateRootLCACategory(environmentId, rootCategoryId) {
+private def getOrCreateRootLCACategory(rootCategoryId) {
   result = sql.firstRow("SELECT ID FROM DATA_CATEGORY WHERE PATH = 'lca' AND ENVIRONMENT_ID = 2 AND DATA_CATEGORY_ID = " + rootCategoryId);
   if (result != null) {
     return result.ID;
