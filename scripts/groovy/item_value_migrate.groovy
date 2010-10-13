4c0564bf85d892089957b9ab9b77cdcfb25c28e6
David Keen <david.keen@amee.com>
2010-10-13 17:38:08 +0100

    Deal with histories. PL-3302.

--- scripts/groovy/item_value_migrate.groovy
+++ scripts/groovy/item_value_migrate.groovy
@@ -14,6 +14,10 @@ def numberValueBatch = 1000
 def numberValueBatchCount = 0
 def textValueBatch = 1000
 def textValueBatchCount = 0
+def numberValueHistoryBatch = 1000
+def numberValueHistoryBatchCount = 0
+def textValueHistoryBatch = 1000
+def textValueHistoryBatchCount = 0
 
 def valueTypes = ["UNSPECIFIED", "TEXT", "DATE", "BOOLEAN", "INTEGER", "DECIMAL"]
 
@@ -143,11 +147,11 @@ sql.commit()
 
 // Create a view for the data item values
 sql.execute "CREATE OR REPLACE VIEW data_item_values AS " +
-    "SELECT iv.ID, iv.UID, iv.STATUS, iv.VALUE, iv.CREATED, iv.MODIFIED, i.ID as DATA_ITEM_ID, iv.ITEM_VALUE_DEFINITION_ID, vd.VALUE_TYPE, iv.UNIT, iv.PER_UNIT " +
+    "SELECT iv.ID, iv.UID, iv.STATUS, iv.VALUE, iv.CREATED, iv.MODIFIED, i.ID as DATA_ITEM_ID, iv.ITEM_VALUE_DEFINITION_ID, vd.VALUE_TYPE, iv.UNIT, iv.PER_UNIT, iv.START_DATE " +
     "FROM ITEM_VALUE AS iv JOIN ITEM i ON iv.ITEM_ID = i.ID " +
     "JOIN ITEM_VALUE_DEFINITION ivd ON iv.ITEM_VALUE_DEFINITION_ID = ivd.ID " +
     "JOIN VALUE_DEFINITION vd on ivd.VALUE_DEFINITION_ID = vd.ID " +
-    "WHERE i.TYPE = 'DI'"
+    "WHERE i.TYPE = 'DI' LIMIT 10000"
 
 def dataItemValues = sql.dataSet('data_item_values')
 
@@ -161,6 +165,16 @@ def dataItemTextValueSql =
     "VALUES (?, ?, ?, ?, ?, ?, ?, ?)"
 def dataItemTextValueStatement = sql.connection.prepareStatement(dataItemTextValueSql)
 
+def dataItemNumberValueHistorySql =
+    "INSERT INTO DATA_ITEM_NUMBER_VALUE_HISTORY (ID, UID, STATUS, VALUE, CREATED, MODIFIED, ITEM_VALUE_DEFINITION_ID, DATA_ITEM_ID, UNIT, PER_UNIT, START_DATE) " +
+    "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
+def dataItemNumberValueHistoryStatement = sql.connection.prepareStatement(dataItemNumberValueHistorySql)
+
+def dataItemTextValueHistorySql =
+    "INSERT INTO DATA_ITEM_TEXT_VALUE_HISTORY (ID, UID, STATUS, VALUE, CREATED, MODIFIED, DATA_ITEM_ID, ITEM_VALUE_DEFINITION_ID, START_DATE) " +
+    "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)"
+def dataItemTextValueHistoryStatement = sql.connection.prepareStatement(dataItemTextValueHistorySql)
+
 dataItemValues.each { row ->
 
     if (row.VALUE_TYPE == valueTypes.indexOf("INTEGER") ||
@@ -168,26 +182,57 @@ dataItemValues.each { row ->
 
         // Handle numbers
         try {
-            dataItemNumberValueStatement.with {
-                setObject(1, row.ID)
-                setObject(2, row.UID)
-                setObject(3, row.STATUS)
-                setObject(4, Double.parseDouble(row.VALUE))
-                setObject(5, row.CREATED)
-                setObject(6, row.MODIFIED)
-                setObject(7, row.DATA_ITEM_ID)
-                setObject(8, row.ITEM_VALUE_DEFINITION_ID)
-                setObject(9, (row.UNIT ? row.UNIT : ''))
-                setObject(10, (row.PER_UNIT ? row.PER_UNIT : ''))
 
-                addBatch()
-                numberValueBatchCount++
+            if (row.START_DATE.toString().equals('1970-01-01 00:00:00.0')) {
 
-                if (numberValueBatchCount >= numberValueBatch) {
-                    // Execute this batch.
-                    executeBatch()
-                    println "Created ${numberValueBatch} DATA_ITEM_NUMBER_VALUEs in a batch."
-                    numberValueBatchCount = 0
+                // Add to standard table
+                dataItemNumberValueStatement.with {
+                    setObject(1, row.ID)
+                    setObject(2, row.UID)
+                    setObject(3, row.STATUS)
+                    setObject(4, Double.parseDouble(row.VALUE))
+                    setObject(5, row.CREATED)
+                    setObject(6, row.MODIFIED)
+                    setObject(7, row.DATA_ITEM_ID)
+                    setObject(8, row.ITEM_VALUE_DEFINITION_ID)
+                    setObject(9, (row.UNIT ? row.UNIT : ''))
+                    setObject(10, (row.PER_UNIT ? row.PER_UNIT : ''))
+
+                    addBatch()
+                    numberValueBatchCount++
+
+                    if (numberValueBatchCount >= numberValueBatch) {
+                        // Execute this batch.
+                        executeBatch()
+                        println "Created ${numberValueBatch} DATA_ITEM_NUMBER_VALUEs in a batch."
+                        numberValueBatchCount = 0
+                    }
+                }
+            } else {
+
+                // Add to history table
+                dataItemNumberValueHistoryStatement.with {
+                    setObject(1, row.ID)
+                    setObject(2, row.UID)
+                    setObject(3, row.STATUS)
+                    setObject(4, Double.parseDouble(row.VALUE))
+                    setObject(5, row.CREATED)
+                    setObject(6, row.MODIFIED)
+                    setObject(7, row.DATA_ITEM_ID)
+                    setObject(8, row.ITEM_VALUE_DEFINITION_ID)
+                    setObject(9, (row.UNIT ? row.UNIT : ''))
+                    setObject(10, (row.PER_UNIT ? row.PER_UNIT : ''))
+                    setObject(11, row.START_DATE)
+
+                    addBatch()
+                    numberValueHistoryBatchCount++
+
+                    if (numberValueHistoryBatchCount >= numberValueHistoryBatch) {
+                        // Execute this batch.
+                        executeBatch()
+                        println "Created ${numberValueHistoryBatch} DATA_ITEM_NUMBER_VALUE_HISTORYs in a batch."
+                        numberValueHistoryBatchCount = 0
+                    }
                 }
             }
         } catch (NumberFormatException e) {
@@ -197,37 +242,67 @@ dataItemValues.each { row ->
     } else {
 
         // Handle text
-        dataItemTextValueStatement.with {
-            setObject(1, row.ID)
-            setObject(2, row.UID)
-            setObject(3, row.STATUS)
+        if (row.START_DATE.toString().equals('1970-01-01 00:00:00.0')) {
+            dataItemTextValueStatement.with {
+                setObject(1, row.ID)
+                setObject(2, row.UID)
+                setObject(3, row.STATUS)
 
-            // Truncate any strings > 255
-            value = row.VALUE.toString()
-            if (value.size() > 255) {
-                println "Truncating DATA_ITEM string value. ID: ${row.ID}, OLD VALUE: ${value}, NEW VALUE: ${value[0..254]}"
-                setObject(4, value[0..254])
-            } else {
-                setObject(4, value)
+                // Truncate any strings > 255
+                value = row.VALUE.toString()
+                if (value.size() > 255) {
+                    System.err.println "Truncating DATA_ITEM string value. ID: ${row.ID}, OLD VALUE: ${value}, NEW VALUE: ${value[0..254]}"
+                    setObject(4, value[0..254])
+                } else {
+                    setObject(4, value)
+                }
+                setObject(5, row.CREATED)
+                setObject(6, row.MODIFIED)
+                setObject(7, row.DATA_ITEM_ID)
+                setObject(8, row.ITEM_VALUE_DEFINITION_ID)
+
+                addBatch()
+                textValueBatchCount++
+
+                if (textValueBatchCount >= textValueBatch) {
+                    // Execute this batch.
+                    executeBatch()
+                    println "Created ${textValueBatch} DATA_ITEM_TEXT_VALUEs in a batch."
+                    textValueBatchCount = 0
+                }
             }
-            setObject(5, row.CREATED)
-            setObject(6, row.MODIFIED)
-            setObject(7, row.DATA_ITEM_ID)
-            setObject(8, row.ITEM_VALUE_DEFINITION_ID)
+        } else {
+            dataItemTextValueHistoryStatement.with {
+                setObject(1, row.ID)
+                setObject(2, row.UID)
+                setObject(3, row.STATUS)
 
-            addBatch()
-            textValueBatchCount++
+                // Truncate any strings > 255
+                value = row.VALUE.toString()
+                if (value.size() > 255) {
+                    System.err.println "Truncating DATA_ITEM string value. ID: ${row.ID}, OLD VALUE: ${value}, NEW VALUE: ${value[0..254]}"
+                    setObject(4, value[0..254])
+                } else {
+                    setObject(4, value)
+                }
+                setObject(5, row.CREATED)
+                setObject(6, row.MODIFIED)
+                setObject(7, row.DATA_ITEM_ID)
+                setObject(8, row.ITEM_VALUE_DEFINITION_ID)
+                setObject(9, row.START_DATE)
 
-            if (textValueBatchCount >= textValueBatch) {
-                // Execute this batch.
-                executeBatch()
-                println "Created ${textValueBatch} DATA_ITEM_TEXT_VALUEs in a batch."
-                textValueBatchCount = 0
+                addBatch()
+                textValueHistoryBatchCount++
+
+                if (textValueHistoryBatchCount >= textValueHistoryBatch) {
+                    // Execute this batch.
+                    executeBatch()
+                    println "Created ${textValueHistoryBatch} DATA_ITEM_TEXT_VALUE_HISTORYs in a batch."
+                    textValueHistoryBatchCount = 0
+                }
             }
         }
     }
-
-
 }
 
 // Handle remaining Item Values in current batch.
@@ -240,6 +315,16 @@ if (textValueBatchCount > 0) {
     dataItemTextValueStatement.executeBatch()
     println "Created ${textValueBatchCount} DATA_ITEM_TEXT_VALUEs in a batch."
     textValueBatchCount = 0
+}
+if (numberValueHistoryBatchCount > 0) {
+    dataItemNumberValueHistoryStatement.executeBatch()
+    println "Created ${numberValueHistoryBatchCount} DATA_ITEM_NUMBER_VALUE_HISTORYs in a batch."
+    numberValueHistoryBatchCount = 0
+}
+if (textValueHistoryBatchCount > 0) {
+    dataItemTextValueHistoryStatement.executeBatch()
+    println "Created ${textValueHistoryBatchCount} DATA_ITEM_TEXT_VALUE_HISTORYs in a batch."
+    textValueHistoryBatchCount = 0
 }
 
 sql.commit()
