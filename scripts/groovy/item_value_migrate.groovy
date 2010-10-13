403c65e97b975bd1cbd58ab0bccb021ca9b868d3
David Keen <david.keen@amee.com>
2010-10-13 11:53:13 +0100

    Use the VALUE_DEFINITION to determine whether a value is text or number. PL-3302.

--- scripts/groovy/item_value_migrate.groovy
+++ scripts/groovy/item_value_migrate.groovy
@@ -1,7 +1,21 @@
+/* You will probably need to increase the java heap space before running this script:
+ * export JAVA_OPTS='-Xmx1G'
+ *
+ * To redirect stdout and stderr:
+ * groovy item_value_migrate.groovy 1> out.log 2> err.log
+ *
+ * This script requires the MySQL connector. Copy the jar to ~/.groovy/lib/
+ *
+ */
+
 import groovy.sql.Sql
 
 def numberValueBatch = 1000
+def numberValueBatchCount = 0
 def textValueBatch = 1000
+def textValueBatchCount = 0
+
+def valueTypes = ["UNSPECIFIED", "TEXT", "DATE", "BOOLEAN", "INTEGER", "DECIMAL"]
 
 // Handle command line parameters
 def cli = configureCliBuilder()
@@ -27,12 +41,13 @@ sql.connection.autoCommit = false
 
 // Create a view for the profile item values
 sql.execute "CREATE OR REPLACE VIEW profile_item_values AS " +
-    "SELECT iv.ID, iv.UID, iv.STATUS, iv.VALUE, iv.CREATED, iv.MODIFIED, i.ID as PROFILE_ITEM_ID, " +
-    "iv.ITEM_VALUE_DEFINITION_ID, iv.UNIT, iv.PER_UNIT FROM ITEM_VALUE AS iv JOIN ITEM i ON iv.ITEM_ID = i.ID WHERE i.TYPE = 'PI'"
+    "SELECT iv.ID, iv.UID, iv.STATUS, iv.VALUE, iv.CREATED, iv.MODIFIED, i.ID as PROFILE_ITEM_ID, iv.ITEM_VALUE_DEFINITION_ID, vd.VALUE_TYPE, iv.UNIT, iv.PER_UNIT " +
+    "FROM ITEM_VALUE AS iv JOIN ITEM i ON iv.ITEM_ID = i.ID " +
+    "JOIN ITEM_VALUE_DEFINITION ivd ON iv.ITEM_VALUE_DEFINITION_ID = ivd.ID " +
+    "JOIN VALUE_DEFINITION vd on ivd.VALUE_DEFINITION_ID = vd.ID " +
+    "WHERE i.TYPE = 'PI'"
 
 def profileItemValues = sql.dataSet('profile_item_values')
-def numberValueBatchCount = 0
-def textValueBatchCount = 0
 
 def profileItemNumberValueSql =
     "INSERT INTO PROFILE_ITEM_NUMBER_VALUE (ID, UID, STATUS, VALUE, CREATED, MODIFIED, PROFILE_ITEM_ID, ITEM_VALUE_DEFINITION_ID, UNIT, PER_UNIT) " +
@@ -46,30 +61,35 @@ def profileItemTextValueStatement = sql.connection.prepareStatement(profileItemT
 
 profileItemValues.each { row ->
 
-    if (row.VALUE.toString().isDouble()) {
+    if (row.VALUE_TYPE == valueTypes.indexOf("INTEGER") ||
+        row.VALUE_TYPE == valueTypes.indexOf("DECIMAL")) {
 
         // Handle numbers
-        profileItemNumberValueStatement.with {
-            setObject(1, row.ID)
-            setObject(2, row.UID)
-            setObject(3, row.STATUS)
-            setObject(4, Double.parseDouble(row.VALUE))
-            setObject(5, row.CREATED)
-            setObject(6, row.MODIFIED)
-            setObject(7, row.PROFILE_ITEM_ID)
-            setObject(8, row.ITEM_VALUE_DEFINITION_ID)
-            setObject(9, (row.UNIT ? row.UNIT : ''))
-            setObject(10, (row.PER_UNIT ? row.PER_UNIT : ''))
+        try {
+            profileItemNumberValueStatement.with {
+                setObject(1, row.ID)
+                setObject(2, row.UID)
+                setObject(3, row.STATUS)
+                setObject(4, Double.parseDouble(row.VALUE))
+                setObject(5, row.CREATED)
+                setObject(6, row.MODIFIED)
+                setObject(7, row.PROFILE_ITEM_ID)
+                setObject(8, row.ITEM_VALUE_DEFINITION_ID)
+                setObject(9, (row.UNIT ? row.UNIT : ''))
+                setObject(10, (row.PER_UNIT ? row.PER_UNIT : ''))
 
-            addBatch()
-            numberValueBatchCount++
+                addBatch()
+                numberValueBatchCount++
 
-            if (numberValueBatchCount >= numberValueBatch) {
-                // Execute this batch.
-                executeBatch()
-                println "Created ${numberValueBatch} PROFILE_ITEM_NUMBER_VALUEs in a batch."
-                numberValueBatchCount = 0
+                if (numberValueBatchCount >= numberValueBatch) {
+                    // Execute this batch.
+                    executeBatch()
+                    println "Created ${numberValueBatch} PROFILE_ITEM_NUMBER_VALUEs in a batch."
+                    numberValueBatchCount = 0
+                }
             }
+        } catch (NumberFormatException e) {
+            System.err.println "Error parsing PROFILE_ITEM value as double. ID: ${row.ID}, VALUE: ${row.VALUE}. ${e.getMessage()}"
         }
     } else {
         
@@ -82,6 +102,7 @@ profileItemValues.each { row ->
             // Truncate any strings > 255
             value = row.VALUE.toString()
             if (value.size() > 255) {
+                println "Truncating PROFILE_ITEM string value. ID: ${row.ID}, OLD VALUE: ${value}, NEW VALUE: ${value[0..254]}"
                 setObject(4, value[0..254])
             } else {
                 setObject(4, value)
@@ -102,8 +123,6 @@ profileItemValues.each { row ->
             }
         }
     }
-
-
 }
 
 // Handle remaining Item Values in current batch.
@@ -123,8 +142,11 @@ sql.commit()
 
 // Create a view for the data item values
 sql.execute "CREATE OR REPLACE VIEW data_item_values AS " +
-    "SELECT iv.ID, iv.UID, iv.STATUS, iv.VALUE, iv.CREATED, iv.MODIFIED, i.ID as DATA_ITEM_ID, " +
-    "iv.ITEM_VALUE_DEFINITION_ID, iv.UNIT, iv.PER_UNIT FROM ITEM_VALUE AS iv JOIN ITEM i ON iv.ITEM_ID = i.ID WHERE i.TYPE = 'DI'"
+    "SELECT iv.ID, iv.UID, iv.STATUS, iv.VALUE, iv.CREATED, iv.MODIFIED, i.ID as DATA_ITEM_ID, iv.ITEM_VALUE_DEFINITION_ID, vd.VALUE_TYPE, iv.UNIT, iv.PER_UNIT " +
+    "FROM ITEM_VALUE AS iv JOIN ITEM i ON iv.ITEM_ID = i.ID " +
+    "JOIN ITEM_VALUE_DEFINITION ivd ON iv.ITEM_VALUE_DEFINITION_ID = ivd.ID " +
+    "JOIN VALUE_DEFINITION vd on ivd.VALUE_DEFINITION_ID = vd.ID " +
+    "WHERE i.TYPE = 'DI' LIMIT 10000"
 
 def dataItemValues = sql.dataSet('data_item_values')
 
@@ -138,98 +160,37 @@ def dataItemTextValueSql =
     "VALUES (?, ?, ?, ?, ?, ?, ?, ?)"
 def dataItemTextValueStatement = sql.connection.prepareStatement(dataItemTextValueSql)
 
-//def query = "SELECT * FROM data_item_values WHERE ID > ? LIMIT 1000"
-//def maxId = 0
-//def more = true
-//def executeQuery = {
-//    def oldMaxId = maxId
-//    sql.eachRow(query, [maxId]) { row ->
-//
-//        println row.ID
-//
-//        if (row.VALUE.toString().isDouble()) {
-//
-//            // Handle numbers
-//            dataItemNumberValueStatement.with {
-//                setObject(1, row.ID)
-//                setObject(2, row.UID)
-//                setObject(3, row.STATUS)
-//                setObject(4, row.VALUE)
-//                setObject(5, row.CREATED)
-//                setObject(6, row.MODIFIED)
-//                setObject(7, row.DATA_ITEM_ID)
-//                setObject(8, row.ITEM_VALUE_DEFINITION_ID)
-//                setObject(9, (row.UNIT ? row.UNIT : ''))
-//                setObject(10, (row.PER_UNIT ? row.PER_UNIT : ''))
-//
-//                addBatch()
-//                numberValueBatchCount++
-//
-//                if (numberValueBatchCount >= numberValueBatch) {
-//                    // Execute this batch.
-//                    executeBatch()
-//                    println "Created ${numberValueBatch} DATA_ITEM_NUMBER_VALUEs in a batch."
-//                    numberValueBatchCount = 0
-//                }
-//            }
-//        } else {
-//
-//            // Handle text
-//            dataItemTextValueStatement.with {
-//                setObject(1, row.ID)
-//                setObject(2, row.UID)
-//                setObject(3, row.STATUS)
-//                setObject(4, row.VALUE)
-//                setObject(5, row.CREATED)
-//                setObject(6, row.MODIFIED)
-//                setObject(7, row.DATA_ITEM_ID)
-//                setObject(8, row.ITEM_VALUE_DEFINITION_ID)
-//
-//                addBatch()
-//                textValueBatchCount++
-//
-//                if (textValueBatchCount >= textValueBatch) {
-//                    // Execute this batch.
-//                    executeBatch()
-//                    println "Created ${textValueBatch} DATA_ITEM_TEXT_VALUEs in a batch."
-//                    textValueBatchCount = 0
-//                }
-//            }
-//        }
-//
-//        maxId = row.ID
-//    }
-//    more = maxId != oldMaxId
-//}
-//
-//while (more) { executeQuery() }
-
 dataItemValues.each { row ->
 
-    if (row.VALUE.toString().isDouble()) {
+    if (row.VALUE_TYPE == valueTypes.indexOf("INTEGER") ||
+        row.VALUE_TYPE == valueTypes.indexOf("DECIMAL")) {
 
         // Handle numbers
-        dataItemNumberValueStatement.with {
-            setObject(1, row.ID)
-            setObject(2, row.UID)
-            setObject(3, row.STATUS)
-            setObject(4, Double.parseDouble(row.VALUE))
-            setObject(5, row.CREATED)
-            setObject(6, row.MODIFIED)
-            setObject(7, row.DATA_ITEM_ID)
-            setObject(8, row.ITEM_VALUE_DEFINITION_ID)
-            setObject(9, (row.UNIT ? row.UNIT : ''))
-            setObject(10, (row.PER_UNIT ? row.PER_UNIT : ''))
+        try {
+            dataItemNumberValueStatement.with {
+                setObject(1, row.ID)
+                setObject(2, row.UID)
+                setObject(3, row.STATUS)
+                setObject(4, Double.parseDouble(row.VALUE))
+                setObject(5, row.CREATED)
+                setObject(6, row.MODIFIED)
+                setObject(7, row.DATA_ITEM_ID)
+                setObject(8, row.ITEM_VALUE_DEFINITION_ID)
+                setObject(9, (row.UNIT ? row.UNIT : ''))
+                setObject(10, (row.PER_UNIT ? row.PER_UNIT : ''))
 
-            addBatch()
-            numberValueBatchCount++
+                addBatch()
+                numberValueBatchCount++
 
-            if (numberValueBatchCount >= numberValueBatch) {
-                // Execute this batch.
-                executeBatch()
-                println "Created ${numberValueBatch} DATA_ITEM_NUMBER_VALUEs in a batch."
-                numberValueBatchCount = 0
+                if (numberValueBatchCount >= numberValueBatch) {
+                    // Execute this batch.
+                    executeBatch()
+                    println "Created ${numberValueBatch} DATA_ITEM_NUMBER_VALUEs in a batch."
+                    numberValueBatchCount = 0
+                }
             }
+        } catch (NumberFormatException e) {
+            System.err.println "Error parsing DATA_ITEM value as double. ID: ${row.ID}, VALUE: ${row.VALUE}. ${e.getMessage()}"
         }
     } else {
 
@@ -240,9 +201,9 @@ dataItemValues.each { row ->
             setObject(3, row.STATUS)
 
             // Truncate any strings > 255
-            // TODO: log the original string
             value = row.VALUE.toString()
             if (value.size() > 255) {
+                println "Truncating DATA_ITEM string value. ID: ${row.ID}, OLD VALUE: ${value}, NEW VALUE: ${value[0..254]}"
                 setObject(4, value[0..254])
             } else {
                 setObject(4, value)
