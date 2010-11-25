8b23876d816b25282f91acd8484c028189105c07
David Keen <david.keen@amee.com>
2010-11-25 17:37:01 +0000

    PL-6588 - added chmod +x to munin scripts.

--- deploy/plugins/amee/recipes/amee.rb
+++ deploy/plugins/amee/recipes/amee.rb
@@ -27,14 +27,14 @@ namespace :amee do
   end
   
   task :munin_symlink do
-    sudo "ln -s #{current_path}/munin/jmx_ /etc/munin/plugins/jmx_amee_data_source"
-    sudo "ln -s #{current_path}/munin/jmx_ /etc/munin/plugins/jmx_amee_profiles"
-    sudo "ln -s #{current_path}/munin/jmx_ /etc/munin/plugins/jmx_java_cpu"
-    sudo "ln -s #{current_path}/munin/jmx_ /etc/munin/plugins/jmx_java_hibernate_entity"
-    sudo "ln -s #{current_path}/munin/jmx_ /etc/munin/plugins/jmx_java_threads"
-    sudo "ln -s #{current_path}/munin/jstat__gccount /etc/munin/plugins/jstat_gccount"
-    sudo "ln -s #{current_path}/munin/jstat__gctime /etc/munin/plugins/jstat_gctime"
-    sudo "ln -s #{current_path}/munin/jstat__heap /etc/munin/plugins/jstat_heap"
+    sudo "ln -s #{current_path}/munin/jmx_ /etc/munin/plugins/jmx_amee_data_source && chmod +x /etc/munin/plugins/jmx_amee_data_source"
+    sudo "ln -s #{current_path}/munin/jmx_ /etc/munin/plugins/jmx_amee_profiles && chmod +x /etc/munin/plugins/jmx_amee_profiles"
+    sudo "ln -s #{current_path}/munin/jmx_ /etc/munin/plugins/jmx_java_cpu && chmod +x /etc/munin/plugins/jmx_java_cpu"
+    sudo "ln -s #{current_path}/munin/jmx_ /etc/munin/plugins/jmx_java_hibernate_entity && chmod +x /etc/munin/plugins/jmx_java_hibernate_entity"
+    sudo "ln -s #{current_path}/munin/jmx_ /etc/munin/plugins/jmx_java_threads && chmod +x /etc/munin/plugins/jmx_java_threads"
+    sudo "ln -s #{current_path}/munin/jstat__gccount /etc/munin/plugins/jstat_gccount && chmod +x /etc/munin/plugins/jstat_gccount"
+    sudo "ln -s #{current_path}/munin/jstat__gctime /etc/munin/plugins/jstat_gctime && chmod +x /etc/munin/plugins/jstat_gctime"
+    sudo "ln -s #{current_path}/munin/jstat__heap /etc/munin/plugins/jstat_heap && chmod +x /etc/munin/plugins/jstat_heap"
   end
   
 
