828112638a820fc1b4f2e21c089c126cefbddcd8
David Keen <david.keen@amee.com>
2011-01-12 16:57:10 +0000

    OPS-741. Handle infinite and nan for data item values.

--- scripts/groovy/item_value_migrate.groovy
+++ scripts/groovy/item_value_migrate.groovy
@@ -258,7 +258,7 @@ while (rs.next()) {
                     setObject(2, rs.getString("UID"))
                     setObject(3, rs.getInt("STATUS"))
                     def rowVal = rs.getString("VALUE")
-                    if (rowVal == 'Infinity' || rowVal == 'NaN') {
+                    if (Double.parseDouble(rowVal).infinite || Double.parseDouble(rowVal).naN) {
                         throw new NumberFormatException()
                     }
                     setObject(4, rowVal == "" || rowVal == "-" ? null : Double.parseDouble(rowVal))
