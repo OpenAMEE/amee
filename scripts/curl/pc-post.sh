05a4bfb3981bab835441bab1d358e12da8e1bc2d
Diggory Briercliffe <dig@amee.cc>
2011-03-30 22:17:34 +0100

    PL-10605 - Profile Item number values need to default to null rather than 0.0.

--- scripts/curl/pc-post.sh
+++ scripts/curl/pc-post.sh
@@ -3,14 +3,24 @@
 
 . curl.conf
 
-startDate="2013-08-02T16:08:00%2B0000"
-distance="24.0"
-tyres="false"
-occupants="2"
+# Local
+amee_profile="UOE3JAKWFPFW"
 
-data="dataItemUid=9A9E8852220B&validFrom=20090701&distanceKmPerMonth=428.62872985519&name=trip_754537_start&end=false"
+# Science
+#amee_profile="Z0R4M6CJHEVN"
+
+# /transport/car/generic
+#startDate="2013-08-02T16:08:00%2B0000"
+#distance="24.0"
+#tyres="false"
+#occupants="2"
+#data="dataItemUid=9A9E8852220B&validFrom=20090701&distanceKmPerMonth=428.62872985519&name=trip_754537_start&end=false"
 #data="dataItemUid=5CF2E792CFEA&validFrom=20090310&long2=101.7&name=trip_511961_finish&lat1=3.16667;end=true;long1=101.7;lat2=3.16667
-amee_url="profiles/${amee_profile}/transport/car/generic"
+
+# /business/processes/fugitive/mining/underground/BA7676732036
+data="dataItemUid=BA7676732036&coalProduction=5000&name=qqqqq"
+
+amee_url="profiles/${amee_profile}/business/processes/fugitive/mining/underground"
 
 type="xml"
 
