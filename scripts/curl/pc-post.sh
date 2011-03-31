cb0e906ea82d2efa44c017af134bf1367378062f
Diggory Briercliffe <dig@amee.cc>
2011-03-31 10:48:34 +0100

    PL-10644 - Implement DataItem.getItemValues to mimic old code for algorithms.

--- scripts/curl/pc-post.sh
+++ scripts/curl/pc-post.sh
@@ -18,9 +18,12 @@ amee_profile="UOE3JAKWFPFW"
 #data="dataItemUid=5CF2E792CFEA&validFrom=20090310&long2=101.7&name=trip_511961_finish&lat1=3.16667;end=true;long1=101.7;lat2=3.16667
 
 # /business/processes/fugitive/mining/underground/BA7676732036
-data="dataItemUid=BA7676732036&coalProduction=5000&name=qqqqq"
+#data="dataItemUid=BA7676732036&coalProduction=5000&name=qqqqq"
 
-amee_url="profiles/${amee_profile}/business/processes/fugitive/mining/underground"
+# /home/appliances/cooking/oven/BA7676732036
+data="dataItemUid=4D7B3366536C&numberOfPeople=3&name=q11qqqq"
+
+amee_url="profiles/${amee_profile}/home/appliances/cooking/oven"
 
 type="xml"
 
