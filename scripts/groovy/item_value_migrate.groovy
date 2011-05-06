3f3912cf6ebf337c1953958ccfe534f83d70a257
David Keen <david.keen@amee.com>
2011-05-06 12:49:31 +0100

    Fix quoted dates in migration script. INFRA-648.

--- scripts/groovy/item_value_migrate.groovy
+++ scripts/groovy/item_value_migrate.groovy
@@ -106,7 +106,7 @@ if (replace) {
 }
 def profileItemTextValueStatement = sqlInsert.connection.prepareStatement(profileItemTextValueSql)
 
-def rs = st.executeQuery("SELECT ID, UID, STATUS, VALUE, CREATED, MODIFIED, PROFILE_ITEM_ID, ITEM_VALUE_DEFINITION_ID, VALUE_TYPE, UNIT, PER_UNIT FROM profile_item_values WHERE MODIFIED >= ${from}")
+def rs = st.executeQuery("SELECT ID, UID, STATUS, VALUE, CREATED, MODIFIED, PROFILE_ITEM_ID, ITEM_VALUE_DEFINITION_ID, VALUE_TYPE, UNIT, PER_UNIT FROM profile_item_values WHERE MODIFIED >= '${from}'")
 
 while (rs.next()) {
     rowValType = rs.getInt("VALUE_TYPE")
@@ -290,7 +290,7 @@ if (replace) {
 }
 def dataItemTextValueHistoryStatement = sqlInsert.connection.prepareStatement(dataItemTextValueHistorySql)
 
-rs = st.executeQuery("SELECT ID, UID, STATUS, VALUE, CREATED, MODIFIED, DATA_ITEM_ID, ITEM_VALUE_DEFINITION_ID, VALUE_TYPE, UNIT, PER_UNIT, START_DATE FROM data_item_values WHERE MODIFIED >= ${from}")
+rs = st.executeQuery("SELECT ID, UID, STATUS, VALUE, CREATED, MODIFIED, DATA_ITEM_ID, ITEM_VALUE_DEFINITION_ID, VALUE_TYPE, UNIT, PER_UNIT, START_DATE FROM data_item_values WHERE MODIFIED >= '${from}'")
 
 while (rs.next()) {
     rowValType = rs.getInt("VALUE_TYPE")
