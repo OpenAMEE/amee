49e132cacedf1bd8f0457667a9c4a2e1b223d883
David Keen <david.keen@amee.com>
2011-05-13 10:00:37 +0100

    Add ORDER BY statements to migration selects so data is in primary key order. INFRA-648.

--- scripts/groovy/item_migrate.groovy
+++ scripts/groovy/item_migrate.groovy
@@ -78,7 +78,7 @@ if (replace) {
 
 def profileItemStatement = sqlInsert.connection.prepareStatement(profileItemSql)
 
-def rs = st.executeQuery("SELECT ID, UID, NAME, CREATED, MODIFIED, START_DATE, END_DATE, ITEM_DEFINITION_ID, DATA_ITEM_ID, PROFILE_ID, STATUS, DATA_CATEGORY_ID FROM ITEM WHERE TYPE = 'PI' AND MODIFIED >= '${from}'")
+def rs = st.executeQuery("SELECT ID, UID, NAME, CREATED, MODIFIED, START_DATE, END_DATE, ITEM_DEFINITION_ID, DATA_ITEM_ID, PROFILE_ID, STATUS, DATA_CATEGORY_ID FROM ITEM WHERE TYPE = 'PI' AND MODIFIED >= '${from}' ORDER BY ID")
 while (rs.next()) {
     profileItemStatement.with {
         setObject(1, rs.getLong("ID"))
@@ -141,7 +141,7 @@ if (replace) {
 
 def dataItemStatement = sqlInsert.connection.prepareStatement(dataItemSql)
 
-rs = st.executeQuery("SELECT ID, UID, NAME, PATH, CREATED, MODIFIED, ITEM_DEFINITION_ID, DATA_CATEGORY_ID, STATUS FROM ITEM WHERE TYPE = 'DI' AND MODIFIED >= '${from}'")
+rs = st.executeQuery("SELECT ID, UID, NAME, PATH, CREATED, MODIFIED, ITEM_DEFINITION_ID, DATA_CATEGORY_ID, STATUS FROM ITEM WHERE TYPE = 'DI' AND MODIFIED >= '${from}' ORDER BY ID")
 while (rs.next()) {
     dataItemStatement.with {
         setObject(1, rs.getLong("ID"))
