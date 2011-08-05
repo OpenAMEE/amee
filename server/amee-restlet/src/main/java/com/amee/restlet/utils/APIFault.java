3b9e4802badab954633c9b686ecd7818fb4c4f4f
David Keen <david.keen@amee.com>
2011-08-05 10:16:07 +0100

    Refactor enum. PL-10635.

--- server/amee-restlet/src/main/java/com/amee/restlet/utils/APIFault.java
+++ server/amee-restlet/src/main/java/com/amee/restlet/utils/APIFault.java
@@ -2,51 +2,41 @@ package com.amee.restlet.utils;
 
 public enum APIFault {
 
-    NONE,
-    INVALID_PARAMETERS,
-    INVALID_API_PARAMETERS,
-    INVALID_CONTENT,
-    MISSING_PARAMETERS,
-    INVALID_DATE_FORMAT,
-    INVALID_DATE_RANGE,
-    INVALID_PRORATA_REQUEST,
-    DUPLICATE_ITEM,
-    INVALID_UNIT,
-    EMPTY_LIST,
-    ENTITY_NOT_FOUND,
-    MAX_BATCH_SIZE_EXCEEDED,
-    DELETE_MUST_LEAVE_AT_LEAST_ONE_ITEM_VALUE,
-    INVALID_RESOURCE_MODIFICATION,
-    NOT_AUTHORIZED_FOR_INDIRECT_ACCESS;
+    NONE(0, ""),
+    INVALID_PARAMETERS(1, "A request was received with one or more invalid parameters."),
+    INVALID_API_PARAMETERS(2, "A request was received with one or more parameters not supported by the version of the API."),
+    INVALID_CONTENT(3, "A request was received with invalid content."),
+    MISSING_PARAMETERS(4, "A request was received with one or more missing parameters."),
+    INVALID_DATE_FORMAT(5, "A request was received with one or more datetime parameters having an invalid format."),
+    INVALID_DATE_RANGE(6, "A request was received with an invalid date range."),
+    INVALID_PRORATA_REQUEST(7, "A prorata request was received without a recognised bounded date range."),
+    DUPLICATE_ITEM(8, "A POST or PUT request was received which would have resulted in a duplicate resource being created."),
+    INVALID_UNIT(9, "A request was received with an invalid unit."),
+    EMPTY_LIST(10, "An empty list was received or produced."),
+    ENTITY_NOT_FOUND(11, "An entity was not found for the given identifier."),
+    MAX_BATCH_SIZE_EXCEEDED(12, "Max batch size was exceeded."),
+    DELETE_MUST_LEAVE_AT_LEAST_ONE_ITEM_VALUE(13, "The DELETE operation must leave at least one ITEM_VALUE per ITEM_VALUE_DEFINTION."),
+    INVALID_RESOURCE_MODIFICATION(14, "A POST or PUT request was received which would have resulted in an invalid resource modification."),
+    NOT_AUTHORIZED_FOR_INDIRECT_ACCESS(15, "A request was received which is not authorized to access an entity indirectly");
 
-    private String[] strings = {
-            "", // NONE
-            "A request was received with one or more invalid parameters.", // INVALID_PARAMETERS
-            "A request was received with one or more parameters not supported by the version of the API.", // INVALID_API_PARAMETERS
-            "A request was received with invalid content.", // INVALID_CONTENT
-            "A request was received with one or more missing parameters.", // MISSING_PARAMETERS
-            "A request was received with one or more datetime parameters having an invalid format.", // INVALID_DATE_FORMAT
-            "A request was received with an invalid date range.", // INVALID_DATE_RANGE
-            "A prorata request was received without a recognised bounded date range.", // INVALID_PRORATA_REQUEST
-            "A POST or PUT request was received which would have resulted in a duplicate resource being created.", // DUPLICATE_ITEM
-            "A request was received with an invalid unit.", // INVALID_UNIT
-            "An empty list was received or produced.", // EMPTY_LIST
-            "An entity was not found for the given identifier.", // ENTITY_NOT_FOUND
-            "Max batch size was exceeded.", // MAX_BATCH_SIZE_EXCEEDED
-            "The DELETE operation must leave at least one ITEM_VALUE per ITEM_VALUE_DEFINTION.", // DELETE_MUST_LEAVE_AT_LEAST_ONE_ITEM_VALUE
-            "A POST or PUT request was received which would have resulted in an invalid resource modification.", // INVALID_RESOURCE_MODIFICATION
-            "A request was received which is not authorized to access an entity indirectly", // NOT_AUTHORIZED_FOR_INDIRECT_ACCESS
-    };
+    private final int code;
+    private final String message;
 
-    public String getString() {
-        return strings[this.ordinal()];
+    APIFault(int code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
     }
 
     public String getCode() {
-        return Integer.toString(this.ordinal());
+        return Integer.toString(code);
     }
 
+    @Override
     public String toString() {
-        return getString();
+        return getMessage();
     }
 }
