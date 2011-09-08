78f37f2d5d1b3fb611d913aeaa00ccfdb2e05a52
David Keen <david.keen@amee.com>
2011-09-08 11:57:34 +0100

    Restrict User resource access to ADMIN. PL-11031.

--- server/amee-admin/src/main/java/com/amee/admin/restlet/environment/AdminResource.java
+++ server/amee-admin/src/main/java/com/amee/admin/restlet/environment/AdminResource.java
@@ -1,17 +1,38 @@
 package com.amee.admin.restlet.environment;
 
-import com.amee.restlet.BaseResource;
+import com.amee.domain.IAMEEEntityReference;
+import com.amee.domain.auth.AccessSpecification;
+import com.amee.domain.auth.PermissionEntry;
+import com.amee.restlet.AuthorizeResource;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 @Component("adminResource")
 @Scope("prototype")
-public class AdminResource extends BaseResource implements Serializable {
+public class AdminResource extends AuthorizeResource implements Serializable {
 
     @Override
     public String getTemplatePath() {
         return "admin.ftl";
+    }
+
+    @Override
+    public List<IAMEEEntityReference> getEntities() {
+        List<IAMEEEntityReference> entities = new ArrayList<IAMEEEntityReference>();
+        entities.add(getRootDataCategory());
+        return entities;
+    }
+
+    @Override
+    public List<AccessSpecification> getGetAccessSpecifications() {
+        List<AccessSpecification> accessSpecifications = new ArrayList<AccessSpecification>();
+        for (IAMEEEntityReference entity : getDistinctEntities()) {
+            accessSpecifications.add(new AccessSpecification(entity, PermissionEntry.ADMIN));
+        }
+        return accessSpecifications;
     }
 }
