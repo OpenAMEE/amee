9b5bf19996fa302c62839539273df049b0400c75
David Keen <david.keen@amee.com>
2011-10-04 12:37:30 +0100

    Move various resources to AdminResources. PL-11031.

--- server/amee-admin/src/main/java/com/amee/admin/restlet/environment/ItemDefinitionsResource.java
+++ server/amee-admin/src/main/java/com/amee/admin/restlet/environment/ItemDefinitionsResource.java
@@ -1,10 +1,9 @@
-package com.amee.restlet.environment;
+package com.amee.admin.restlet.environment;
 
 import com.amee.domain.IAMEEEntityReference;
 import com.amee.domain.Pager;
-import com.amee.domain.ValueDefinition;
-import com.amee.domain.ValueType;
-import com.amee.restlet.AuthorizeResource;
+import com.amee.domain.data.ItemDefinition;
+import com.amee.restlet.environment.DefinitionBrowser;
 import com.amee.service.data.DataConstants;
 import com.amee.service.definition.DefinitionService;
 import org.apache.commons.logging.Log;
@@ -24,14 +23,13 @@ import org.springframework.stereotype.Component;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
-import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 @Component
 @Scope("prototype")
-public class ValueDefinitionsResource extends AuthorizeResource implements Serializable {
+public class ItemDefinitionsResource extends AdminResource {
 
     private final Log log = LogFactory.getLog(getClass());
 
@@ -41,7 +39,7 @@ public class ValueDefinitionsResource extends AuthorizeResource implements Seria
     @Autowired
     private DefinitionBrowser definitionBrowser;
 
-    private ValueDefinition newValueDefinition;
+    private ItemDefinition newItemDefinition;
 
     @Override
     public void initialise(Context context, Request request, Response response) {
@@ -57,18 +55,17 @@ public class ValueDefinitionsResource extends AuthorizeResource implements Seria
 
     @Override
     public String getTemplatePath() {
-        return DataConstants.VIEW_VALUE_DEFINITIONS;
+        return DataConstants.VIEW_ITEM_DEFINITIONS;
     }
 
     @Override
     public Map<String, Object> getTemplateValues() {
         Pager pager = getPager();
-        List<ValueDefinition> valueDefinitions = definitionService.getValueDefinitions(pager);
+        List<ItemDefinition> itemDefinitions = definitionService.getItemDefinitions(pager);
         pager.setCurrentPage(getPage());
         Map<String, Object> values = super.getTemplateValues();
         values.put("browser", definitionBrowser);
-        values.put("valueDefinitions", valueDefinitions);
-        values.put("valueTypes", ValueType.getChoices());
+        values.put("itemDefinitions", itemDefinitions);
         values.put("pager", pager);
         return values;
     }
@@ -78,37 +75,35 @@ public class ValueDefinitionsResource extends AuthorizeResource implements Seria
         JSONObject obj = new JSONObject();
         if (isGet()) {
             Pager pager = getPager();
-            List<ValueDefinition> valueDefinitions = definitionService.getValueDefinitions(pager);
+            List<ItemDefinition> itemDefinitions = definitionService.getItemDefinitions(pager);
             pager.setCurrentPage(getPage());
-            JSONArray valueDefinitionsJSONArray = new JSONArray();
-            for (ValueDefinition valueDefinition : valueDefinitions) {
-                valueDefinitionsJSONArray.put(valueDefinition.getJSONObject(false));
+            JSONArray itemDefinitionsJSONArray = new JSONArray();
+            for (ItemDefinition itemDefinition : itemDefinitions) {
+                itemDefinitionsJSONArray.put(itemDefinition.getJSONObject(false));
             }
-            obj.put("valueDefinitions", valueDefinitionsJSONArray);
-            obj.put("valueTypes", ValueType.getJSONObject());
+            obj.put("itemDefinitions", itemDefinitionsJSONArray);
             obj.put("pager", pager.getJSONObject());
         } else if (getRequest().getMethod().equals(Method.POST)) {
-            obj.put("valueDefinition", newValueDefinition.getJSONObject());
+            obj.put("itemDefinition", newItemDefinition.getJSONObject());
         }
         return obj;
     }
 
     @Override
     public Element getElement(Document document) {
-        Element element = document.createElement("ValueDefinitionsResource");
+        Element element = document.createElement("ItemDefinitionsResource");
         if (isGet()) {
             Pager pager = getPager();
-            List<ValueDefinition> valueDefinitions = definitionService.getValueDefinitions(pager);
+            List<ItemDefinition> itemDefinitions = definitionService.getItemDefinitions(pager);
             pager.setCurrentPage(getPage());
-            Element valueDefinitionsElement = document.createElement("ValueDefinitions");
-            for (ValueDefinition valueDefinition : valueDefinitions) {
-                valueDefinitionsElement.appendChild(valueDefinition.getElement(document, false));
+            Element itemDefinitionsElement = document.createElement("ItemDefinitions");
+            for (ItemDefinition itemDefinition : itemDefinitions) {
+                itemDefinitionsElement.appendChild(itemDefinition.getElement(document, false));
             }
-            element.appendChild(valueDefinitionsElement);
-            element.appendChild(ValueType.getElement(document));
+            element.appendChild(itemDefinitionsElement);
             element.appendChild(pager.getElement(document));
         } else if (getRequest().getMethod().equals(Method.POST)) {
-            element.appendChild(newValueDefinition.getElement(document));
+            element.appendChild(newItemDefinition.getElement(document));
         }
         return element;
     }
@@ -122,19 +117,15 @@ public class ValueDefinitionsResource extends AuthorizeResource implements Seria
     public void doAccept(Representation entity) {
         log.debug("doAccept()");
         Form form = getForm();
-        if ((form.getFirstValue("name") != null) && (form.getFirstValue("valueType") != null)) {
-            String valueType = form.getFirstValue("valueType");
-            valueType = valueType.equalsIgnoreCase("DECIMAL") ? "DOUBLE" : valueType;
-            newValueDefinition = new ValueDefinition(
-                    form.getFirstValue("name"),
-                    ValueType.valueOf(valueType));
-            definitionService.persist(newValueDefinition);
+        if (form.getFirstValue("name") != null) {
+            newItemDefinition = new ItemDefinition(form.getFirstValue("name"));
+            definitionService.persist(newItemDefinition);
         }
-        if (newValueDefinition != null) {
+        if (newItemDefinition != null) {
             if (isStandardWebBrowser()) {
                 success();
             } else {
-                // return a response for API calls
+                // Return a response for API calls
                 super.handleGet();
             }
         } else {
