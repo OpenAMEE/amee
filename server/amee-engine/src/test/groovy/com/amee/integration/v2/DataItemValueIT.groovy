fe56beaafd063acaceb88cc9754c3f5dc463f8ce
conan <conan.cook@amee.com>
2012-03-23 10:42:54 +0000

    add XML versions of Data Item Value integration tests PL-11198

--- server/amee-engine/src/test/groovy/com/amee/integration/v2/DataItemValueIT.groovy
+++ server/amee-engine/src/test/groovy/com/amee/integration/v2/DataItemValueIT.groovy
@@ -16,24 +16,15 @@ class DataItemValueIT extends BaseApiTest {
 
     def paths = [
         "country",
-        "energyPerTime",
-        "responsibleArea",
-        "totalArea",
         "massCH4PerEnergy",
         "massCO2PerEnergy",
-        "massN2OPerEnergy"
+        "massN2OPerEnergy",
+        "source"
     ]
 
     /**
-     * Test creating a DataItem and check that:
-     * <ul>
-     *  <li>DataItemValue defaults are created correctly</li>
-     *  <li>Specified DataItemValues are created correctly</li>
-     *  <li>A DataItemValueHistory can be created by POST</li>
-     *  <li>A DataItemValueHistory can be created by PUT</li>
-     *  <li>
-     *  
-     * </ul> 
+     * Test creating a DataItem and check that DataItemValue defaults and specified values
+     * are created correctly, and that history items can be created and deleted correctly.
      */
     @Test
     void createUpdateDataItemValueJson() {
@@ -53,11 +44,10 @@ class DataItemValueIT extends BaseApiTest {
 
         // Check default Data Item Values were created
         def responseGet = client.get(
-            path: "/data/business/energy/electricity",
+            path: "/data/business/energy/electricity/" + uid,
             contentType: JSON)
         assert SUCCESS_OK.code == responseGet.status
-        def dataItemValuePaths = responseGet.data.itemValues.collect{it.path}
-        dataItemValuePaths.each { assert paths.contains(it) }
+        assert responseGet.data.dataItem.itemValues.collect{it.path}.sort() == paths
 
         // Check specified Data Item Value was created
         responseGet = client.get(
@@ -140,6 +130,117 @@ class DataItemValueIT extends BaseApiTest {
         }
     }
     
+    /**
+    * Test creating a DataItem and check that DataItemValue defaults and specified values
+    * are created correctly, and that history items can be created and deleted correctly.
+    */
+   @Test
+   void createUpdateDataItemValueXML() {
+       setAdminUser()
+
+       // Create a new DataItem
+       def responsePost = client.post(
+           path: "/data/business/energy/electricity",
+           body: [
+               newObjectType: 'DI',
+               country: "Test Country"],
+           requestContentType: URLENC,
+           contentType: XML)
+       assert SUCCESS_CREATED.code == responsePost.status
+       def location = responsePost.headers['Location'].value
+       def uid = location.split("/")[7]
+
+       // Check default Data Item Values were created
+       def responseGet = client.get(
+           path: "/data/business/energy/electricity/" + uid,
+           contentType: XML)
+       assert SUCCESS_OK.code == responseGet.status
+       assert responseGet.data.DataItemResource.DataItem.ItemValues.ItemValue.Path*.text().sort() == paths
+
+       // Check specified Data Item Value was created
+       responseGet = client.get(
+           path: "/data/business/energy/electricity/" + uid + "/country",
+           contentType: XML)
+       assert SUCCESS_OK.code == responseGet.status
+       assert responseGet.data.DataItemValueResource.ItemValue.Value.text() == 'Test Country'
+       
+       // Create a new Data Item Value History
+       responsePost = client.post(
+           path: "/data/business/energy/electricity/" + uid,
+           body: [
+               massCO2PerEnergy: '100',
+               startDate: '2000-01-01T00:00:00Z'],
+           requestContentType: URLENC,
+           contentType: XML)
+       assert SUCCESS_CREATED.code == responsePost.status
+       
+       // Check new history item was created
+       responseGet = client.get(
+           path: "/data/business/energy/electricity/" + uid + "/massCO2PerEnergy",
+           query: [
+               startDate: '2000-01-01T00:00:00Z',
+               endDate: '2001-01-01T00:00:00Z'],
+           contentType: XML)
+       assert SUCCESS_OK.code == responseGet.status
+       assert responseGet.data.DataItemValueResource.ItemValue.Value.text() == '100'
+       
+       // Update the new Data Item Value History
+       def responsePut = client.put(
+           path: "/data/business/energy/electricity/" + uid + "/massCO2PerEnergy",
+           body: [
+               value: "42",
+               startDate: '2000-01-01T00:00:00Z'],
+           requestContentType: URLENC,
+           contentType: XML)
+       assert SUCCESS_OK.code == responsePut.status
+       
+       // Check new history item was created
+       responseGet = client.get(
+           path: "/data/business/energy/electricity/" + uid + "/massCO2PerEnergy",
+           query: [
+               startDate: '2000-01-01T00:00:00Z',
+               endDate: '2001-01-01T00:00:00Z'],
+           contentType: XML)
+       assert SUCCESS_OK.code == responseGet.status
+       assert responseGet.data.DataItemValueResource.ItemValue.Value.text() == '42'
+       
+       // Delete the history item
+       def responseDelete = client.delete(
+           path: "/data/business/energy/electricity/" + uid + "/massCO2PerEnergy",
+           contentType: XML)
+       assert SUCCESS_OK.code == responseDelete.status
+       
+       // Check the history item was deleted
+       try{
+           responseGet = client.get(
+               path: "/data/business/energy/electricity/" + uid + "/massCO2PerEnergy",
+               query: [
+                   startDate: '2001-01-01T00:00:00Z',
+                   endDate: '2002-01-01T00:00:00Z'],
+               contentType: XML)
+       }catch(HttpResponseException e) {
+           assert CLIENT_ERROR_NOT_FOUND.code == e.response.status
+       }
+       
+       // Delete the Data Item
+       responseDelete = client.delete(
+           path: "/data/business/energy/electricity/" + uid,
+           contentType: XML)
+       assert SUCCESS_OK.code == responseDelete.status
+       
+       // Check the Data Item was deleted
+       try{
+           responseGet = client.get(
+               path: "/data/business/energy/electricity/" + uid,
+               contentType: XML)
+       }catch(HttpResponseException e) {
+           assert CLIENT_ERROR_NOT_FOUND.code == e.response.status
+       }
+   }
+    
+    /**
+     * Tests that a data series can be created and edited and that it responds correctly.
+     */
     @Test
     void dataItemSeriesJson() {
         setAdminUser()
@@ -245,4 +346,113 @@ class DataItemValueIT extends BaseApiTest {
             assert CLIENT_ERROR_NOT_FOUND.code == e.response.status
         }
     }
+    
+    /**
+    * Tests that a data series can be created and edited and that it responds correctly.
+    */
+   @Test
+   void dataItemSeriesXML() {
+       setAdminUser()
+       
+       // Create a new DataItem
+       def responsePost = client.post(
+           path: "/data/business/energy/electricity",
+           body: [
+               newObjectType: 'DI',
+               country: "Test Country"],
+           requestContentType: URLENC,
+           contentType: XML)
+       assert SUCCESS_CREATED.code == responsePost.status
+       def location = responsePost.headers['Location'].value
+       def uid = location.split("/")[7]
+       
+       // Create a data series with a new value each day
+       responsePost = client.post(
+           path: "/data/business/energy/electricity/" + uid,
+           body: [
+               massCO2PerEnergy: '1',
+               startDate: '2000-01-01T00:00:00Z'],
+           requestContentType: URLENC,
+           contentType: XML)
+       assert SUCCESS_CREATED.code == responsePost.status
+       
+       responsePost = client.post(
+           path: "/data/business/energy/electricity/" + uid,
+           body: [
+               massCO2PerEnergy: '2',
+               startDate: '2000-01-02T00:00:00Z'],
+           requestContentType: URLENC,
+           contentType: XML)
+       assert SUCCESS_CREATED.code == responsePost.status
+       
+       responsePost = client.post(
+           path: "/data/business/energy/electricity/" + uid,
+           body: [
+               massCO2PerEnergy: '3',
+               startDate: '2000-01-03T00:00:00Z'],
+           requestContentType: URLENC,
+           contentType: XML)
+       assert SUCCESS_CREATED.code == responsePost.status
+       
+       // Check the history items are all available
+       def responseGet = client.get(
+           path: "/data/business/energy/electricity/" + uid + "/massCO2PerEnergy",
+           query: [startDate: '2000-01-01T12:00:00Z'],
+           contentType: XML)
+       assert SUCCESS_OK.code == responseGet.status
+       assert '1' == responseGet.data.DataItemValueResource.ItemValue.Value.text()
+       
+       responseGet = client.get(
+           path: "/data/business/energy/electricity/" + uid + "/massCO2PerEnergy",
+            query: [startDate: '2000-01-02T12:00:00Z'],
+           contentType: XML)
+       assert SUCCESS_OK.code == responseGet.status
+       assert '2' == responseGet.data.DataItemValueResource.ItemValue.Value.text()
+       
+       responseGet = client.get(
+           path: "/data/business/energy/electricity/" + uid + "/massCO2PerEnergy",
+            query: [startDate: '2000-01-03T12:00:00Z'],
+           contentType: XML)
+       assert SUCCESS_OK.code == responseGet.status
+       assert '3' == responseGet.data.DataItemValueResource.ItemValue.Value.text()
+       
+       responseGet = client.get(
+           path: "/data/business/energy/electricity/" + uid + "/massCO2PerEnergy",
+            query: [
+                startDate: '2000-01-01T12:00:00Z',
+                valuesPerPage: '10'],
+           contentType: XML)
+       assert SUCCESS_OK.code == responseGet.status
+       assert responseGet.data.DataItemValueResource.ItemValues.ItemValue.size() == 4 // 3 we made plus default
+       
+       // Delete one in the middle
+       def responseDelete = client.delete(
+           path: "/data/business/energy/electricity/" + uid + "/massCO2PerEnergy",
+           query: [startDate: '2000-01-02T12:00:00Z'],
+           contentType: XML)
+       assert SUCCESS_OK.code == responseDelete.status
+       
+       // Check the previous result is now returned for that time
+       responseGet = client.get(
+           path: "/data/business/energy/electricity/" + uid + "/massCO2PerEnergy",
+            query: [startDate: '2000-01-02T12:00:00Z'],
+           contentType: XML)
+       assert SUCCESS_OK.code == responseGet.status
+       assert '1' == responseGet.data.DataItemValueResource.ItemValue.Value.text()
+       
+       // Delete the Data Item
+       responseDelete = client.delete(
+           path: "/data/business/energy/electricity/" + uid,
+           contentType: XML)
+       assert SUCCESS_OK.code == responseDelete.status
+       
+       // Check the Data Item was deleted
+       try{
+           responseGet = client.get(
+               path: "/data/business/energy/electricity/" + uid,
+               contentType: XML)
+       }catch(HttpResponseException e) {
+           assert CLIENT_ERROR_NOT_FOUND.code == e.response.status
+       }
+   }
 }
